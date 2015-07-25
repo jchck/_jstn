f2f99c1901295fde3b98dea4b1cd7464b3540b4c
jchck <hey@justinchick.com>
2015-07-25 15:26:00 -0500

    query 1 post at a time

--- front-page.php
+++ front-page.php
@@ -1,9 +1,18 @@
-<div class="latest-post text-center">
-	<div class="container-fluid wrap">
-		<h3>latest post title</h3>
-	<p>Blandit varius ut praesent nascetur eu penatibus nisi risus faucibus nunc ornare adipiscing nunc adipiscing. Condimentum turpis massa.</p>
-	</div>
-</div>
 <?php while (have_posts()) : the_post(); ?>
+	<div class="latest-post text-center">
+		<div class="container-fluid wrap">
+			<?php 
+				$blog       = $wp_query;
+				$wp_query   = null;
+				$wp_query   = new WP_Query(); $wp_query->query('posts_per_page=1');
+				while ($wp_query->have_posts()) : $wp_query->the_post();
+		    ?>
+			<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			<p><?php the_excerpt(); ?></p>
+			<?php endwhile; ?>
+			<?php wp_reset_query(); ?>
+		</div>
+	</div>
+
 	<?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
