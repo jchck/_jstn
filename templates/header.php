7a720db773ad4f9452fa95484db39513639d7ca4
jchck <hey@justinchick.com>
2015-07-25 15:07:19 -0500

    default page header

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,12 @@
-<header class="banner container-fluid" role="banner">
+<header class="banner wrap container-fluid" role="banner">
   <div class="row center-xs">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php get_template_part( 'templates/logo' ); ?></a>
   </div>
+  <?php if (is_front_page()) { ?>
+    <div class="row center-xs">
+      <h2>Blandit varius ut praesent nascetur eu penatibus nisi risus.</h2>
+    </div>
+  <?} ?>
   <nav class="row center-xs" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
