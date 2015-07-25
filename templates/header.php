643e8574a209be51f1ed9adc9e1a03935d618e1b
jchck <hey@justinchick.com>
2015-07-25 14:08:40 -0500

    include logo as svg

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 <header class="banner container-fluid" role="banner">
   <div class="row center-xs">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php get_template_part( 'templates/logo' ); ?></a>
     <nav role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
