16d779c06da7e9b2649a5191420fe92a4239dcb9
jchck <hey@justinchick.com>
2015-07-25 15:44:57 -0500

    query for the image thumbnail

--- front-page.php
+++ front-page.php
@@ -1,18 +1,20 @@
-<?php while (have_posts()) : the_post(); ?>
-	<div class="latest-post text-center">
-		<div class="container-fluid wrap">
-			<?php 
-				$blog       = $wp_query;
-				$wp_query   = null;
-				$wp_query   = new WP_Query(); $wp_query->query('posts_per_page=1');
-				while ($wp_query->have_posts()) : $wp_query->the_post();
-		    ?>
+<?php while (have_posts()) : the_post(); ?>	
+	<?php 
+		$blog       = $wp_query;
+		$wp_query   = null;
+		$wp_query   = new WP_Query(); $wp_query->query('posts_per_page=1');
+		while ($wp_query->have_posts()) : $wp_query->the_post();
+
+		$img_src 	= wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 'full' ), false, '' ); 
+    ?>
+    <div class="latest-post text-center" style="background-image: url(<?php echo $img_src[0]; ?>)">
+    	<div class="container-fluid wrap">
 			<h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
 			<p><?php the_excerpt(); ?></p>
-			<?php endwhile; ?>
-			<?php wp_reset_query(); ?>
 		</div>
 	</div>
+	<?php endwhile; ?>
+	<?php wp_reset_query(); ?>
 
 	<?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
