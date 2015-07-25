fb0efe072042c4ce2966715112758667ec187e30
jchck <hey@justinchick.com>
2015-07-25 14:15:41 -0500

    set header nav to nav-pill

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,12 @@
 <header class="banner container-fluid" role="banner">
   <div class="row center-xs">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php get_template_part( 'templates/logo' ); ?></a>
-    <nav role="navigation">
+  </div>
+  <nav class="row center-xs" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-pills']);
       endif;
       ?>
     </nav>
-  </div>
 </header>
