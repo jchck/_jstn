042851a5ea4c9dbc6780cb25d2ee234b75f61c72
jchck <hey@justinchick.com>
2015-07-25 15:28:04 -0500

    add link title

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@
 				$wp_query   = new WP_Query(); $wp_query->query('posts_per_page=1');
 				while ($wp_query->have_posts()) : $wp_query->the_post();
 		    ?>
-			<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			<h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
 			<p><?php the_excerpt(); ?></p>
 			<?php endwhile; ?>
 			<?php wp_reset_query(); ?>
