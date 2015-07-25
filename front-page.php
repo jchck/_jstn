af2f3be22a5f4cc47bdf631d411665fa1e48e000
jchck <hey@justinchick.com>
2015-07-25 15:20:26 -0500

    begin latest post banner styles and static content

--- front-page.php
+++ front-page.php
@@ -1,4 +1,9 @@
+<div class="latest-post text-center">
+	<div class="container-fluid wrap">
+		<h3>latest post title</h3>
+	<p>Blandit varius ut praesent nascetur eu penatibus nisi risus faucibus nunc ornare adipiscing nunc adipiscing. Condimentum turpis massa.</p>
+	</div>
+</div>
 <?php while (have_posts()) : the_post(); ?>
-	
 	<?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
