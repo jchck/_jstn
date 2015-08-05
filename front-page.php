22b2dac22342c8461d439e00e1babd3c2b764f4e
jchck <hey@justinchick.com>
2015-08-05 14:53:57 -0500

    updating

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
 		$wp_query   = new WP_Query(); $wp_query->query('posts_per_page=1');
 		while ($wp_query->have_posts()) : $wp_query->the_post();
     ?>
-    <div class="latest-post text-center" style="color: <?php the_field('heading_color') ?>; background-image: url(<?php the_field('featured_image') ?>)">
+    <div class="latest-post text-center" style="color: <?php the_field('heading_text_color') ?>; background-image: url(<?php the_field('banner_image') ?>)">
     	<div class="container-fluid wrap">
 			<a href="<?php echo the_permalink(); ?>"><h2 data-heading class="text-capitalize"><?php the_title(); ?></h2 ></a>
 			<?php the_excerpt(); ?>
