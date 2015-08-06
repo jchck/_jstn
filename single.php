abafe68611186321964accc0b3445b236dcec236
jchck <hey@justinchick.com>
2015-08-06 12:10:43 -0500

    moar

--- single.php
+++ single.php
@@ -1,1 +1,3 @@
-<?php get_template_part('templates/content-single', get_post_type()); ?>
+<div class="container-fluid wrap">
+	<?php get_template_part('templates/content-single', get_post_type()); ?>
+</div>
