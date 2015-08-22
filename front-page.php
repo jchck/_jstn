0a0b74ba7545a3a2d37dc959871b05ccd0be5313
jchck <hey@justinchick.com>
2015-08-22 09:42:17 -0500

    workingout felxboxgrid issues

--- front-page.php
+++ front-page.php
@@ -1,30 +1,8 @@
 <?php while (have_posts()) : the_post(); ?>	
-	<?php 
-		$blog       = $wp_query;
-		$wp_query   = null;
-		$wp_query   = new WP_Query(); $wp_query->query('posts_per_page=1');
-		while ($wp_query->have_posts()) : $wp_query->the_post();
-    ?>
-    <div class="latest-post text-center" style="color: <?php the_field('heading_text_color') ?>; background-image: url(<?php the_field('banner_image') ?>)">
-    	<div class="container-fluid wrap">
-			<a href="<?php echo the_permalink(); ?>"><h2 data-heading class="text-capitalize"><?php the_title(); ?></h2 ></a>
-			<p><a href="<?php echo the_permalink(); ?>" class="btn btn-default">Continue</a></p>
-		</div>
-	</div>
-	<?php endwhile; ?>
-	<?php wp_reset_query(); ?>
 
 	<div class="container-fluid wrap">
-		<div class="row center-xs">
-			<ul class="list-inline">
-				<li>i design</li>
-				<li>i develop</li>
-				<li>i podcast</li>
-				<li>i travel</li>
-			</ul>
-		</div>
 
-		<div class="row">
+		<div class="top-xs row">
 			<div class="col-sm-4">
 				<img src="<?php echo get_template_directory_uri() . '/dist/images/jchck.jpg'; ?>">
 			</div>
