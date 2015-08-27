820dffa2adf2e516c201cb3cef3ca1573ce1efca
jchck <hey@justinchick.com>
2015-08-27 16:24:25 -0500

    use gotham

--- templates/head.php
+++ templates/head.php
@@ -3,6 +3,4 @@
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
-	<script src="//use.typekit.net/miz3brn.js"></script>
-	<script>try{Typekit.load({ async: true });}catch(e){}</script>
 </head>
