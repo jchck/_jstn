b498b289c582347950158d37eae49cace2e90ebe
jchck <hey@justinchick.com>
2015-08-28 14:42:27 -0500

    add a signup

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,24 @@
 <footer class="content-info" role="contentinfo">
 	<div class="container-fluid wrap pt4 pb4 mt4 mb4 border-top-gray">
+		<h3 class="m0 text-center">Keep Up With Me</h3>
+		<p class="m0 text-center">Proin tempus sem ac accumsan dictum.</p>
+		<form>
+			<div class="row center-sm mt4 mb3">
+				<div class="col-xs col-sm-4">
+					<label for="name" class="sr-only">Your Name</label>
+					<input type="text" class="form-control" id="name" placeholder="Your Name">
+				</div>
+				<div class="col-xs col-sm-4">
+					<label for="email" class="sr-only">Your Email</label>
+					<input type="text" class="form-control" id="email" placeholder="Your Email">
+				</div>
+			</div>
+			<div class="row center-xs mb4">
+				<button type="submit" class="btn btn-red">Sign Up</button>
+			</div>
+		</form>
 		<div class="row">
 			<div class="col-xs-12">
-				<h3 class="m0 text-center">Let's Work Together</h3>
-				<p class="mt3 mb4 text-center"><a href="#" class="btn btn-red">Yes Please</a></p>
 				<ul class="list-inline text-center m0">
 					<li><a target="_blank" title="Justin Chick (jchck) on Github" href="https://github.com/jchck"><i class="fa fa-github-alt"></i></a></li>
 					<li><a target="_blank" title="Justin Chick (jchck_) on Twitter" href="https://twitter.com/jchck_"><i class="fa fa-twitter"></i></a></li>
