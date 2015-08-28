82f7e17424b4d1ffd9d4b014d9b5dc29434a49a3
jchck <hey@justinchick.com>
2015-08-28 17:09:34 -0500

    notebook single cards

--- templates/content.php
+++ templates/content.php
@@ -1,7 +1,7 @@
-<article <?php post_class('col-md-4 col-sm-6 col-xs-12'); ?>>
+<article <?php post_class('col-md-4 col-sm-6 col-xs-12 pb1'); ?>>
 	<a href="<?php the_permalink(); ?>" class="post-card p3">
 		<header>
-			<h2 class="m0"><?php the_title(); ?></h2>
+			<h2 class="h3 mt0"><?php the_title(); ?></h2>
 		</header>
 		<div class="entry-summary">
 			<?php the_excerpt(); ?>
