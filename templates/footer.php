491f700f3bd80b7dfb5ba1306a3d861f81f99bee
jchck <hey@justinchick.com>
2015-08-27 21:50:05 -0500

    finalize that ffooter

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,16 @@
 <footer class="content-info" role="contentinfo">
-	<div class="container-fluid wrap pt4 pb4 mt4 border-top-gray">
+	<div class="container-fluid wrap pt4 pb4 mt4 mb4 border-top-gray">
 		<div class="row">
 			<div class="col-xs-12">
 				<h3 class="m0 text-center">Let's Work Together</h3>
+				<p class="mt3 mb4 text-center"><a href="#" class="btn btn-red">Yes Please</a></p>
+				<ul class="list-inline text-center m0">
+					<li><a href="#"><i class="fa fa-github-alt"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+					<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+				</ul>
 			</div>
 		</div>
+		
 	</div>
 </footer>
