3b130782db88e60dc1b50efc932f64fda2c2ab76
jchck <hey@justinchick.com>
2015-08-27 21:54:34 -0500

    social links

--- templates/footer.php
+++ templates/footer.php
@@ -5,9 +5,9 @@
 				<h3 class="m0 text-center">Let's Work Together</h3>
 				<p class="mt3 mb4 text-center"><a href="#" class="btn btn-red">Yes Please</a></p>
 				<ul class="list-inline text-center m0">
-					<li><a href="#"><i class="fa fa-github-alt"></i></a></li>
-					<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-					<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+					<li><a target="_blank" title="Justin Chick (jchck) on Github" href="https://github.com/jchck"><i class="fa fa-github-alt"></i></a></li>
+					<li><a target="_blank" title="Justin Chick (jchck_) on Twitter" href="https://twitter.com/jchck_"><i class="fa fa-twitter"></i></a></li>
+					<li><a target="_blank" title="Justin Chick (jchck_) on Instagram" href="https://instagram.com/jchck_"><i class="fa fa-instagram"></i></a></li>
 				</ul>
 			</div>
 		</div>
