5c22d6cef454dcb604a573698793b29007920285
jchck <hey@justinchick.com>
2015-09-04 18:09:56 -0500

    making some styles available at all breakpoints

--- templates/content.php
+++ templates/content.php
@@ -1,4 +1,4 @@
-<article <?php post_class('col-md-4 col-sm-6 col-xs-12 pb3-md pl2-md pr2-md pb4-xs pl3-xs pr3-xs flex'); ?>>
+<article <?php post_class('col-md-4 col-sm-6 col-xs-12 pb3-md pl2-md pr2-md pb4 pl3 pr3 flex'); ?>>
 	<a href="<?php the_permalink(); ?>" class="post-card p3 flex flex-center">
 		<div class="text-center center-block">
 			<header>
