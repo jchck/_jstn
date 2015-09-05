5c9a34f020a8322cd53619f5cb9391b8a1b225e0
jchck <hey@justinchick.com>
2015-09-05 18:49:04 -0500

    add ability to flip position of iPad via ACF

--- single-work.php
+++ single-work.php
@@ -1,11 +1,16 @@
 <div class="container-fluid wrap">
-	<?php while (have_posts()) : the_post(); ?>
+	<?php while (have_posts()) : the_post();
+
+		$field = get_field_object('device_on_left_or_right');
+		$value = get_field('device_on_left_or_right');
+		$label = $field['choices'][$value];
+	?>
 		<?php get_template_part('templates/page', 'header'); ?>
 		<div class="row middle-sm">
 			<div class="col-sm-6 col-xs-12">
 				<?php get_template_part('templates/content', 'page'); ?>
 			</div>
-			<div class="col-sm-6 last-sm col-xs-12 first-xs">
+			<div class="col-sm-6 <?php echo $label; ?> col-xs-12 first-xs">
 				<div>
 					<div class="ipad">
 						<?php get_template_part( 'templates/ipad' ); ?>
