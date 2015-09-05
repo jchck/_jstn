cb86dbf7c1d0b020f6f731cfeb487eb76627ea9f
jchck <hey@justinchick.com>
2015-09-05 15:52:37 -0500

    functioning static work single template

--- template-custom.php
+++ template-custom.php
@@ -9,16 +9,17 @@
 		<?php get_template_part('templates/page', 'header'); ?>
 		<div class="row middle-sm">
 			<div class="col-sm-6">
-				<p>Vivamus metus felis, viverra eu enim nec, tempus volutpat diam. In ac neque vitae lacus aliquet elementum quis in tellus. Nulla porttitor interdum risus eu pharetra. Donec tortor purus, maximus nec scelerisque eu, sodales eu augue. Cras eget erat non nisi egestas porttitor. Curabitur gravida mauris quis dui convallis porttitor. Nunc lacinia eleifend elementum. Quisque dignissim urna a lorem rutrum, quis pulvinar quam laoreet. Phasellus convallis urna dui, vitae malesuada libero dignissim sit amet. Proin eget tellus in arcu finibus mollis nec ut leo. Proin aliquet auctor ante ac venenatis. Praesent ut elit dignissim, mattis enim non, ultricies tellus. Morbi odio elit, porttitor ut eros id, placerat fermentum neque. Vivamus at varius erat. Donec ac eros ut orci vulputate tempor. Ut sit amet sodales justo, rhoncus gravida diam.
-				</p>
+				<?php get_template_part('templates/content', 'page'); ?>
 			</div>
 			<div class="col-sm-6">
-				<div class="ipad">
-					<?php get_template_part( 'templates/ipad' ); ?>
-					<div class="ipad-screen"></div>
+				<div>
+					<div class="ipad">
+						<?php get_template_part( 'templates/ipad' ); ?>
+						<div class="ipad-screen"></div>
+					</div>
+					<p class="mt3 mb0 text-center"><a href="#" class="btn btn-blue">View Project</a></p>
 				</div>
 			</div>
 		</div>
-		<?php get_template_part('templates/content', 'page'); ?>
 	<?php endwhile; ?>
 </div>
