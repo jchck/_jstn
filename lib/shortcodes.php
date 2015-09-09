fd9075d9468c3b7c11a3256f9cdd7c411cc0b0d1
jchck <hey@justinchick.com>
2015-09-09 16:00:21 -0500

    shortcodes

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -9,6 +9,6 @@ add_shortcode('row', __NAMESPACE__ . '\\row');
 
 
 function half( $atts, $content = null ){
-	return '<div class="col-sm">' . do_shortcode( $content ) . '</div>';
+	return '<div class="col-md col-xs-12">' . do_shortcode( $content ) . '</div>';
 }
 add_shortcode('half', __NAMESPACE__ . '\\half');
