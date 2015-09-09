1e059620bab41c1027cbf87afbda52c522297ed3
jchck <hey@justinchick.com>
2015-09-09 13:53:02 -0500

    hire justin form styles

--- templates/footer.php
+++ templates/footer.php
@@ -1,22 +1,24 @@
 <footer class="content-info" role="contentinfo">
 	<div class="container-fluid wrap pt4 pb4 mt4 mb4 border-top-gray">
-		<h3 class="m0 text-center">Keep Up With Me</h3>
-		<p class="m0 text-center">Sign up and I'll send you a Digital Postcard every week</p>
-		<form>
-			<div class="row center-sm mt4 mb3">
-				<div class="col-xs col-sm-4">
-					<label for="name" class="sr-only">Your Name</label>
-					<input type="text" class="form-control" id="name" placeholder="Your Name">
+		<?php if (!is_page( 'Hire Justin' )) { ?>
+			<h3 class="m0 text-center">Keep Up With Me</h3>
+			<p class="m0 text-center">Sign up and I'll send you a Digital Postcard every week</p>
+			<form>
+				<div class="row center-sm mt4 mb3">
+					<div class="col-xs col-sm-4">
+						<label for="name" class="sr-only">Your Name</label>
+						<input type="text" class="form-control" id="name" placeholder="Your Name">
+					</div>
+					<div class="col-xs col-sm-4">
+						<label for="email" class="sr-only">Your Email</label>
+						<input type="text" class="form-control" id="email" placeholder="Your Email">
+					</div>
 				</div>
-				<div class="col-xs col-sm-4">
-					<label for="email" class="sr-only">Your Email</label>
-					<input type="text" class="form-control" id="email" placeholder="Your Email">
+				<div class="row center-xs mb4">
+					<a type="submit" href="#" class="btn btn-blue">Sign Up</a>
 				</div>
-			</div>
-			<div class="row center-xs mb4">
-				<a type="submit" href="#" class="btn btn-blue">Sign Up</a>
-			</div>
-		</form>
+			</form>
+		<? } ?>
 		<div class="row">
 			<div class="col-xs-12">
 				<ul class="list-inline text-center m0">
