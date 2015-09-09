9280ce4ea30199b6a01bc490095b7a30a6aea1e8
jchck <hey@justinchick.com>
2015-09-09 17:07:28 -0500

    holding

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -17,3 +17,17 @@ function right( $atts, $content = null ){
 	return '<div class="row end-xs"><div class="col-sm-6">' . do_shortcode( $content ) . '</div></div>';
 }
 add_shortcode('right', __NAMESPACE__ . '\\right');
+
+function boxes( $atts, $content) { ?>
+
+	<div class="row">
+		<div class="col-md-6 m4">
+			<p>At a flat project rate</p>
+		</div>
+		<div class="col-md-6 m4"></div>
+	</div>
+
+<? }
+add_shortcode( 'boxes', __NAMESPACE__ . '\\boxes' );
+
+ 
---
+++ templates/hire-boxes.php
@@ -0,0 +1,13 @@
+<?php namespace Roots\Sage\Boxes;
+
+function boxes() { ?>
+
+
+<div class="row">
+	<div class="col-md-6 m4">
+		<p>At a flat project rate</p>
+	</div>
+	<div class="col-md-6 m4"></div>
+</div>
+
+<? }
