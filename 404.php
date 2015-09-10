2b6b249cd9c6ba1a4592a664f4137f3116f2db1b
jchck <hey@justinchick.com>
2015-09-09 21:01:39 -0500

    gif cnetering

--- 404.php
+++ 404.php
@@ -2,11 +2,11 @@
 
 <?php get_template_part('templates/page', 'header'); ?>
 
-<div class="row middle-sm">
-	<div class="col-sm-6">
-		<img src="<?php echo get_template_directory_uri() . '/dist/images/say-what.gif'; ?>">
+<div class="row middle-sm center-xs">
+	<div class="col-sm-6 col-xs-9">
+		<img class="center-block" src="<?php echo get_template_directory_uri() . '/dist/images/say-what.gif'; ?>">
 	</div>
-	<div class="col-sm-6">
+	<div class="col-sm-6 col-xs-9">
 		<p class="m0">Whoa</p>
 		<p class="m0">Easy there, looks like it ain't here man</p>
 		<p class="m0"><a href="<?= esc_url(home_url('/')); ?>">Let's just go on home</a></p>
