61bf28ce3313c346fa1e3057046d5bc63493d293
jchck <hey@justinchick.com>
2015-09-10 18:56:51 -0500

    function to query and return all cat slugs

--- lib/category.php
+++ lib/category.php
@@ -1,7 +1,24 @@
 <?php
 namespace Roots\Sage\Category;
 
-function no_link(){
+/**
+ *
+ * Return the category name, in plain text, no link
+ *
+ */
+function title(){
 	$category = get_the_category();
 	echo $category[0]->cat_name;
+}
+
+/**
+ *
+ * Return as many category slugs are associated with the post
+ * @link https://wordpress.org/support/topic/getting-category-slug-from-posts-in-the-loop
+ *
+ */
+function slug(){
+	foreach (get_the_category() as $category) {
+		echo $category->slug . ' ';
+	}
 }
