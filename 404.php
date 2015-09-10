926c0779ab2687b4a4e290456798def58e69dbf3
jchck <hey@justinchick.com>
2015-09-09 20:59:30 -0500

    404 page styles

--- 404.php
+++ 404.php
@@ -2,10 +2,15 @@
 
 <?php get_template_part('templates/page', 'header'); ?>
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
+<div class="row middle-sm">
+	<div class="col-sm-6">
+		<img src="<?php echo get_template_directory_uri() . '/dist/images/say-what.gif'; ?>">
+	</div>
+	<div class="col-sm-6">
+		<p class="m0">Whoa</p>
+		<p class="m0">Easy there, looks like it ain't here man</p>
+		<p class="m0"><a href="<?= esc_url(home_url('/')); ?>">Let's just go on home</a></p>
+	</div>
 </div>
-
-<?php get_search_form(); ?>
 
 </div>
