7905c5098406d48ddcf5eb98216fe7909b762448
jchck <hey@justinchick.com>
2015-09-09 20:46:07 -0500

    add link to Colophon page

--- templates/footer.php
+++ templates/footer.php
@@ -21,6 +21,7 @@
 		<? } ?>
 		<div class="row">
 			<div class="col-xs-12">
+				<p class="mb0 small text-center"><a href="<?= esc_url(home_url('colophon')); ?>">Colophon</a></p>
 				<ul class="list-inline text-center m0">
 					<li><a target="_blank" title="Justin Chick (jchck) on Github" href="https://github.com/jchck"><i class="i-github"></i></a></li>
 					<li><a target="_blank" title="Justin Chick (jchck_) on Twitter" href="https://twitter.com/jchck_"><i class="i-twitter"></i></a></li>
