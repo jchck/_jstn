f854720a6e6ca0b1a2032c1877055df05494b5ce
jchck <hey@justinchick.com>
2015-09-09 22:04:22 -0500

    remove all google maps references

--- lib/assets.php
+++ lib/assets.php
@@ -70,7 +70,6 @@ function assets() {
 
   wp_enqueue_script('modernizr', asset_path('scripts/modernizr.js'), [], null, true);
 
-  wp_enqueue_script( 'gmaps', '//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false', [], null, true );
   wp_enqueue_script('sage_js', asset_path('scripts/main.js'), ['jquery'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
