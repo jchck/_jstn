777674bf0ea40dc66213550cb6c05743c0fa984a
jchck <hey@justinchick.com>
2015-09-24 14:16:34 -0500

    add margin and sign up btn

--- templates/nav.php
+++ templates/nav.php
@@ -2,8 +2,8 @@
 	<a href="#" class="btn btn-blue m2 visible-xs">Hire Me</a>
 	<?php
 	if (has_nav_menu('primary_navigation')) :
-	  wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+	  wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav mt2']);
 	endif;
 	?>
-	<p class="m0 mt3 text-center border-top"><a href="<?= esc_url(home_url('signup')); ?>" class="btn btn-blue mt3 mb3">Want My Prospective?</a></p>
+	<p class="m0 mt3 text-center border-top hidden-xs"><a href="<?= esc_url(home_url('signup')); ?>" class="btn btn-blue mt3 mb3">Want My Prospective?</a></p>
 </nav>
