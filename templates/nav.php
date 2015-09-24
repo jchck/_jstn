36488919a97277747b656d68e331fc7a25297f7c
jchck <hey@justinchick.com>
2015-09-24 12:16:34 -0500

    navbar customs

--- templates/nav.php
+++ templates/nav.php
@@ -1,8 +1,9 @@
 <nav id="nav" role="navigation">
-	<a href="#" class="btn btn-red mr2 visible-xs">Hire Me</a>
+	<a href="#" class="btn btn-blue m2 visible-xs">Hire Me</a>
 	<?php
 	if (has_nav_menu('primary_navigation')) :
 	  wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
 	endif;
 	?>
+	<p class="m0 mt3 text-center border-top"><a href="<?= esc_url(home_url('signup')); ?>" class="btn btn-blue mt3 mb3">Want My Prospective?</a></p>
 </nav>
