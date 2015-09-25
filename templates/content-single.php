c0e972f284439390942cba2323db3b52503ad566
jchck <hey@justinchick.com>
2015-09-25 13:32:28 -0500

    build optional youtube embed function

--- templates/content-single.php
+++ templates/content-single.php
@@ -15,7 +15,10 @@
         </div>
     </header>
     <div class="entry-content">
-      <?php the_content(); ?>
+      <?php
+        get_template_part( 'templates/video' );
+        the_content();
+      ?>
     </div>
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
---
+++ templates/video.php
@@ -0,0 +1,7 @@
+<?php if (get_field('video')) : ?>
+
+	<div class="embed-responsive embed-responsive-16by9 mb3">
+		<?php the_field('video'); ?>
+	</div>
+
+<? endif; ?>
