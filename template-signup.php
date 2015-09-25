117254c3f603f22e5cad5ed6d1d3ae81b1944f07
jchck <hey@justinchick.com>
2015-09-25 15:15:14 -0500

    load different form

--- template-signup.php
+++ template-signup.php
@@ -5,6 +5,6 @@
 ?>
 <?php while (have_posts()) : the_post(); ?>
 	<div class="container-fluid wrap signup-form">
-		<?php echo do_shortcode( '[gravityform id="2" title="false" description="false"]' ); ?>
+		<?php echo do_shortcode( '[gravityform id="5" title="false" description="false"]' ); ?>
 	</div>
 <?php endwhile; ?>
