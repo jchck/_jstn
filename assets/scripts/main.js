f4e30485cd97026d53b3ddd9a94a06b20d63e46a
jchck <hey@justinchick.com>
2015-09-27 10:15:49 -0500

    uninstall scotch panels

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -21,15 +21,18 @@
         // JavaScript to be fired on all pages
 
         // Scotch Panels
-        $('#nav').scotchPanel({
-          containerSelector: '.hey-scotch',
-          direction: 'right',
-          duration: 200,
-          transition: 'ease',
-          clickSelector: '#toggle-nav',
-          distanceX: '20rem',
-          enableEscapeKey: true
-        });
+        // $('#nav').scotchPanel({
+        //   containerSelector: '.hey-scotch',
+        //   direction: 'right',
+        //   duration: 200,
+        //   transition: 'ease',
+        //   clickSelector: '#toggle-nav',
+        //   distanceX: '20rem',
+        //   enableEscapeKey: true,
+          // useCSS: false,
+          // easingPluginTransition: 'easeInCirc',
+          // useEasingPlugin: true
+        // });
 
       },
       finalize: function() {
