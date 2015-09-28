f0394fafda35e901b7e5fbf1958c0f6c98703a6b
jchck <hey@justinchick.com>
2015-09-28 14:16:55 -0500

    begin filtering gallery content

--- lib/pics.php
+++ lib/pics.php
@@ -8,3 +8,18 @@ function zoomjs($html, $id, $caption, $title, $align, $url){
 	return $img;
 }
 add_action('image_send_to_editor', __NAMESPACE__ . '\\zoomjs', 10, 9);
+
+
+
+function gallery($output = '', $atts, $instance){
+	$return = $output;
+
+	$clean_gallery = clean_gallery( $atts );
+
+	if (!empty($clean_gallery)) {
+		$return = $clean_gallery;
+	}
+	return $return;
+}
+
+add_filter('post_gallery', __NAMESPACE__ . '\\gallery', 10, 3);
