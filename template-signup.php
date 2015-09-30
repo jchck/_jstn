c614c75fa4725b8a953eac12a54d5e666c64a9da
jchck <hey@justinchick.com>
2015-09-30 13:11:26 -0500

    add filler content to signup page

--- template-signup.php
+++ template-signup.php
@@ -5,6 +5,8 @@
 ?>
 <?php while (have_posts()) : the_post(); ?>
 	<div class="container-fluid wrap signup-form">
+		<p>Bla bla bla signup.</p>
+		<p>Bla bla bla you know what to do, right?</p>
 		<?php echo do_shortcode( '[gravityform id="5" title="false" description="false"]' ); ?>
 	</div>
 <?php endwhile; ?>
