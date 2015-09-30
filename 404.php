b9b4d663398f473c86c207651ef25a8aea49cffb
jchck <hey@justinchick.com>
2015-09-30 18:10:41 -0500

    404 2 col margins

--- 404.php
+++ 404.php
@@ -4,12 +4,12 @@
 
 <div class="row middle-sm center-xs">
 	<div class="col-sm-6 col-xs-9">
-		<img class="center-block" src="<?php echo get_template_directory_uri() . '/dist/images/say-what.gif'; ?>">
+		<img class="center-block mr2-sm" src="<?php echo get_template_directory_uri() . '/dist/images/say-what.gif'; ?>">
 	</div>
 	<div class="col-sm-6 col-xs-9">
-		<p class="m0">Whoa</p>
-		<p class="m0">Easy there, looks like it ain't here man</p>
-		<p class="m0"><a href="<?= esc_url(home_url('/')); ?>">Let's just go on home</a></p>
+		<p class="ml2-sm m0">Whoa</p>
+		<p class="ml2-sm m0">Easy there, looks like it ain't here man</p>
+		<p class="ml2-sm m0"><a href="<?= esc_url(home_url('/')); ?>">Let's just go on home</a></p>
 	</div>
 </div>
 
