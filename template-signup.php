350d9dd16e1e434fcef6b244f7ee2fe8cd4dc39b
jchck <hey@justinchick.com>
2015-09-30 20:21:42 -0500

    okay now it's done

--- template-signup.php
+++ template-signup.php
@@ -10,7 +10,7 @@
 				<h1 class="mt0 mb3 lead">Do you want to join the all the amazing people reading my newsletter about living as a digital nomad, freelancing, and creativity every week?</h1>
 				<div class="row center-xs hidden-xs">
 					<div class="col-xs-10">
-						<div class="row">
+						<div class="row middle-sm">
 							<div class="col-sm">
 								<p>Would you like my latest photography (like that shot there) and stories delivered fresh to your inbox every Wednesday morning?</p>
 								<p>Do you like trains, hummus, and/or cats?</p>
@@ -21,7 +21,7 @@
 						</div>
 					</div>
 				</div>
-				<p class="mt3 mb0">Then I'd like for you to answer these two simple questions:</p>
+				<p class="mt3 mb0">If you answered yes <span class="hidden-xs">to any of those,</span> I've got two more questions for you:</p>
 				<?php echo do_shortcode( '[gravityform id="2" title="false" description="false"]' ); ?>
 			</div>
 		</div>
