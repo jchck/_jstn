418b4e56d981455bc65128c143e0fdc8682a4e96
jchck <hey@justinchick.com>
2015-10-01 12:42:54 -0500

    break pre project timeline into partial

--- template-hire.php
+++ template-hire.php
@@ -31,19 +31,7 @@ use Roots\Sage\Date;
 				</a>
 			</div>
 		</div>
-		<h3 class="text-center h3-md">How things run until your project kicks off</h3>
-		<ol>
-			<li>You'll fill out the questionnaire below introducing yourself and your project to me.</li>
-			<li>I'll review and follow up with my availability to grab coffee either in person (if we're in the same city) or via Skype.</li>
-			<li>We get to know each other.  I'll ask you questions to find out what makes you tick, what drives your business, and to explore the project at hand. I expect you to have a few questions of your own.</li>
-			<li>Based on our conversation, I'll follow up with a proposed project scope, timeline, and cost.</li>
-			<li>If necessary, we'll review and I'll answer any questions you have regarding the proposal.</li>
-			<li>We both accept the proposal.</li>
-			<li>We both sign a working agreement.</li>
-			<li>You make a payment of 1/3 the agreed project total cost.</li>
-			<li>We schedule project kickoff. I schedule strictly on a first come, first serve basis.</li>
-			<li>Your project begins on the date scheduled.</li>
-		</ol>
+		<?php get_template_part( 'templates/preproject', 'timeline' ); ?>
 		<div id="lets-go" class="hire-form">
 			<h4 class="text-center hire-cta no-hover h4-md">Does working together sound like something you’d like to do?<br />
 				Then let’s get this ball rolling!
