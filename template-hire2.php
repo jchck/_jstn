f0b0c9b41439ad6d3f539945cce50a88b9bf388f
jchck <hey@justinchick.com>
2015-10-01 14:56:01 -0500

    add calendly via iframe and responsive size it

--- template-hire2.php
+++ template-hire2.php
@@ -9,5 +9,8 @@
 		<h1 class="mt0 text-center h1-md">Boom! First Step Done</h1>
 		<p>That wasn't so hard, right? I can smell something awesome in the works here. Now, on to the next step...</p>
 		<?php get_template_part( 'templates/preproject', 'timeline' ); ?>
+		<div class="embed-responsive embed-responsive-16by9">
+			<iframe class="embed-responsive-item" src="https://calendly.com/justinchick/new-project-intake/"></iframe>
+		</div>
 	</div>
 <?php endwhile; ?>
