c5897441b5b33046864c66164010ef4be02f2711
jchck <hey@justinchick.com>
2015-10-02 10:59:03 -0500

    IE 6+ warning

--- base.php
+++ base.php
@@ -9,7 +9,7 @@ use Roots\Sage\Wrapper;
 <html class="no-js" <?php language_attributes(); ?>>
   <?php get_template_part('templates/head'); ?>
   <body <?php body_class(); ?>>
-    <!--[if lt IE 9]>
+    <!--[if gt IE 6]>
       <div class="alert alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
