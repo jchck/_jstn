512f6ec92a884a59d53a0f3146eeea3c33146d7b
jchck <hey@justinchick.com>
2015-10-11 14:00:35 -0500

    update google verification (again) [deploy: production}

--- templates/head.php
+++ templates/head.php
@@ -2,6 +2,6 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="google-site-verification" content="NeNV9S-5T5QAG4HF7mtD76bKMZMfjw-UpBzHr88GDow" />
+	<meta name="google-site-verification" content="hTg4p_CSh5SZZzRcanNAGKFn8OkPSFFg_Z29uQsQ6uc" />
 	<?php wp_head(); ?>
 </head>
