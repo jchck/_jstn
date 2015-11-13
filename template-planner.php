182da06af941becae18124f1ddb8a377089eb275
jchck <hey@justinchick.com>
2015-11-13 17:52:58 -0600

    v bump

--- template-planner.php
+++ template-planner.php
@@ -9,7 +9,7 @@
 		<h1 class="mt0 mb4 text-center h1-md">Project Planner</h1>
 		<div class="row flex flex-middle">
 			<div class="col-xs-12">
-				<?php echo do_shortcode( '[gravityform id="3" title="false" description="false"]' ); ?>
+				<?php echo do_shortcode( '[gravityform id="6" title="false" description="false"]' ); ?>
 			</div>
 		</div>
 	</div>
