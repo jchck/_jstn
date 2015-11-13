691dede447050b1b138329df67a2d4604b58e4f8
jchck <hey@justinchick.com>
2015-11-12 19:14:45 -0600

    update form id

--- template-signup.php
+++ template-signup.php
@@ -11,7 +11,7 @@
 				<p class="mb0">The map below shows every place I've lived as a digital nomad since beginning this adventure on September 23, 2015.</p>
 				<?php get_template_part( 'templates/the', 'map' ); ?>
 				<p class="mt1 mb0">Do you want to join the others following along and get stories, inspiration, and pictures every Wednesday? </p>
-				<?php echo do_shortcode( '[gravityform id="2" title="false" description="false"]' ); ?>
+				<?php echo do_shortcode( '[gravityform id="5" title="false" description="false"]' ); ?>
 			</div>
 		</div>
 	</div>
