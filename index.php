254b3bb2e6ccb2b00d63ccdd5e3d04bb76afdbd7
jchck <hey@justinchick.com>
2015-11-21 16:21:51 -0600

    404 for archives

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 	<?php get_template_part('templates/page', 'header'); ?>
 
 	<?php if (!have_posts()) : ?>
-		<div class="alert alert-warning">
-			<?php _e('Sorry, no results were found.', 'sage'); ?>
+		<div class="alert alert-warning text-center">
+			<?php _e('Whoops! Nothing here.', 'sage'); ?>
 		</div>
 		<?php get_search_form(); ?>
 	<?php endif; ?>
