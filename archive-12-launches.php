de40e43a1fb96294d2174e2ac27330aa541f3819
jchck <hey@justinchick.com>
2015-12-07 12:17:35 -0500

    hover effects for launch cards

--- archive-12-launches.php
+++ archive-12-launches.php
@@ -5,7 +5,7 @@
 		<!-- Static -->
 		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex'); ?>>
 			<div style="border: 1px solid green;">
-				<a href="#" class="flex middle-xs">
+				<a href="#" class="launch-card flex middle-xs">
 					<img src="http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg" class="pr2" width="190" height="190">
 					<div class="col-xs">
 						<p class="m0">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
