69ffb1210fff106cb2b1a55adcbdd81d548a9a83
jchck <hey@justinchick.com>
2015-12-10 13:47:16 -0500

    plenty of stuff

--- archive-12-launches.php
+++ archive-12-launches.php
@@ -3,94 +3,149 @@
 	<p class="mt4 mb4">Climb leg rub face on everything give attitude nap all day for under the bed. Chase mice attack feet but rub face on everything hopped up on goofballs. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas. Dramatically maintain clicks-and-mortar.</p>
 	<div class="row">
 		<!-- Static Row 1 -->
-		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex'); ?>>
-			<a href="#" class="launch-card flex middle-xs">
-				<img src="http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg" class="mr2" width="150" height="150">
+		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
+			<a href="#" class="launch-card flex middle-xs p2">
+				<?php get_template_part( 'templates/launches', 'jframe' ); ?>
 				<div class="col-xs">
-					<h3 data-heading>jFrame</h3>
-					<p class="m0">A CSS Styleguide</p>
+					<div class="card-text ml2 mr2">
+						<h3 data-heading>jFrame</h3>
+						<p class="m0">A CSS Styleguide</p>
+					</div>
 				</div>
 			</a>
 		</article>
 
-		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex'); ?>>
-			<a href="#" class="launch-card flex middle-xs">
-				<img src="http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg" class="mr2" width="150" height="150">
+		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
+			<a href="#" class="launch-card flex middle-xs p2">
+				<?php get_template_part( 'templates/launches', 'launching' ); ?>
 				<div class="col-xs">
-					<h3 data-heading>Write</h3>
-					<p class="m0">WordPress theme for writers</p>
+					<div class="card-text ml2 mr2">
+						<h3 data-heading>Launching Soon</h3>
+						<p class="m0">TBD</p>
+					</div>
 				</div>
 			</a>
 		</article>
 
-		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex'); ?>>
-			<a href="#" class="launch-card flex middle-xs">
-				<img src="http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg" class="mr2" width="150" height="150">
+		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
+			<a href="#" class="launch-card flex middle-xs p2">
+				<?php get_template_part( 'templates/launches', 'launching' ); ?>
 				<div class="col-xs">
-					<h3 data-heading>Yes</h3>
-					<p class="m0">The most awesome thing yet</p>
+					<div class="card-text ml2 mr2">
+						<h3 data-heading>Launching Soon</h3>
+						<p class="m0">TBD</p>
+					</div>
 				</div>
 			</a>
 		</article>
 
 		<!-- Static Row 2 -->
-		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex'); ?>>
-			<a href="#" class="launch-card flex middle-xs">
-				<img src="http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg" class="mr2" width="150" height="150">
+		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
+			<a href="#" class="launch-card flex middle-xs p2">
+				<?php get_template_part( 'templates/launches', 'launching' ); ?>
 				<div class="col-xs">
-					<h3 data-heading>jFrame</h3>
-					<p class="m0">A CSS Styleguide</p>
+					<div class="card-text ml2 mr2">
+						<h3 data-heading>Launching Soon</h3>
+						<p class="m0">TBD</p>
+					</div>
 				</div>
 			</a>
 		</article>
 
-		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex'); ?>>
-			<a href="#" class="launch-card flex middle-xs">
-				<img src="http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg" class="mr2" width="150" height="150">
+		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
+			<a href="#" class="launch-card flex middle-xs p2">
+				<?php get_template_part( 'templates/launches', 'launching' ); ?>
 				<div class="col-xs">
-					<h3 data-heading>Write</h3>
-					<p class="m0">WordPress theme for writers</p>
+					<div class="card-text ml2 mr2">
+						<h3 data-heading>Launching Soon</h3>
+						<p class="m0">TBD</p>
+					</div>
 				</div>
 			</a>
 		</article>
 
-		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex'); ?>>
-			<a href="#" class="launch-card flex middle-xs">
-				<img src="http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg" class="mr2" width="150" height="150">
+		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
+			<a href="#" class="launch-card flex middle-xs p2">
+				<?php get_template_part( 'templates/launches', 'launching' ); ?>
 				<div class="col-xs">
-					<h3 data-heading>Yes</h3>
-					<p class="m0">The most awesome thing yet</p>
+					<div class="card-text ml2 mr2">
+						<h3 data-heading>Launching Soon</h3>
+						<p class="m0">TBD</p>
+					</div>
 				</div>
 			</a>
 		</article>
 
 		<!-- Static Row 3 -->
-		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex'); ?>>
-			<a href="#" class="launch-card flex middle-xs">
-				<img src="http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg" class="mr2" width="150" height="150">
+		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
+			<a href="#" class="launch-card flex middle-xs p2">
+				<?php get_template_part( 'templates/launches', 'launching' ); ?>
 				<div class="col-xs">
-					<h3 data-heading>jFrame</h3>
-					<p class="m0">A CSS Styleguide</p>
+					<div class="card-text ml2 mr2">
+						<h3 data-heading>Launching Soon</h3>
+						<p class="m0">TBD</p>
+					</div>
 				</div>
 			</a>
 		</article>
 
-		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex'); ?>>
-			<a href="#" class="launch-card flex middle-xs">
-				<img src="http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg" class="mr2" width="150" height="150">
+		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
+			<a href="#" class="launch-card flex middle-xs p2">
+				<?php get_template_part( 'templates/launches', 'launching' ); ?>
 				<div class="col-xs">
-					<h3 data-heading>Write</h3>
-					<p class="m0">WordPress theme for writers</p>
+					<div class="card-text ml2 mr2">
+						<h3 data-heading>Launching Soon</h3>
+						<p class="m0">TBD</p>
+					</div>
 				</div>
 			</a>
 		</article>
 
-		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex'); ?>>
-			<a href="#" class="launch-card flex middle-xs">
-				<img src="http://d2v52k3cl9vedd.cloudfront.net/assets/images/placeholder-square.svg" class="mr2" width="150" height="150">
+		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
+			<a href="#" class="launch-card flex middle-xs p2">
+				<?php get_template_part( 'templates/launches', 'launching' ); ?>
 				<div class="col-xs">
-					<h3 data-heading>Yes</h3>
-					<p class="m0">The most awesome thing yet</p>
+					<div class="card-text ml2 mr2">
+						<h3 data-heading>Launching Soon</h3>
+						<p class="m0">TBD</p>
+					</div>
+				</div>
+			</a>
+		</article>
+
+		<!-- Static Row 4 -->
+		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
+			<a href="#" class="launch-card flex middle-xs p2">
+				<?php get_template_part( 'templates/launches', 'launching' ); ?>
+				<div class="col-xs">
+					<div class="card-text ml2 mr2">
+						<h3 data-heading>Launching Soon</h3>
+						<p class="m0">TBD</p>
+					</div>
+				</div>
+			</a>
+		</article>
+
+		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
+			<a href="#" class="launch-card flex middle-xs p2">
+				<?php get_template_part( 'templates/launches', 'launching' ); ?>
+				<div class="col-xs">
+					<div class="card-text ml2 mr2">
+						<h3 data-heading>Launching Soon</h3>
+						<p class="m0">TBD</p>
+					</div>
+				</div>
+			</a>
+		</article>
+
+		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
+			<a href="#" class="launch-card flex middle-xs p2">
+				<?php get_template_part( 'templates/launches', 'launching' ); ?>
+				<div class="col-xs">
+					<div class="card-text ml2 mr2">
+						<h3 data-heading>Launching Soon</h3>
+						<p class="m0">TBD</p>
+					</div>
 				</div>
 			</a>
 		</article>
