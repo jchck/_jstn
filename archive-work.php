705b14ebdfbea01be429feb61726c815dd834183
jchck <hey@justinchick.com>
2015-12-10 21:26:10 -0500

    more dummy text

--- archive-work.php
+++ archive-work.php
@@ -1,6 +1,6 @@
 <div class="container-fluid wrap-12-launches">
 	<h1 data-heading>Work Portfolio</h1>
-	<p>The professional work of Justin Chick</p>
+	<p>The professional work of Justin Chick Vivamus non egestas mi. Morbi orci lorem, tincidunt ut justo at, interdum volutpat massa. Donec varius orci massa. Cras a sem a lorem aliquet eleifend eget ut augue. Vivamus eu lacus mi. Etiam pretium enim eget lorem dignissim pulvinar. Phasellus facilisis fringilla tempor. </p>
 	<div class="row center-xs">
 		<article <?php post_class('col-md-7 col-sm-6 col-xs-12 flex p0'); ?>>
 			<a href="#" class="launch-card flex middle-xs p2">
