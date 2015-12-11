a1f7653bde9e856a25c450a54d59a6b91e989e10
jchck <hey@justinchick.com>
2015-12-10 19:16:55 -0500

    make front page the same

--- front-page.php
+++ front-page.php
@@ -18,20 +18,39 @@
 
 	<div class="container-fluid wrap">
 
-		<div class="row middle-xs center-xs">
-			<div class="col-sm-6 col-xs-12">
-				<h1 class="lead m0">Hi, I'm Justin Chick. I'm a designer, developer, and digital nomad.</h1>
+		<div class="row middle-xs center-xs between-sm">
+			<div class="col-sm-5 col-xs-12">
+				<h1 class="lead m0">Hi, I'm Justin Chick.<br />Designer, front-end developer, and digital nomad.</h1>
 				<h2 class="lead mb2-sm mb3-md mt0"><a class="highlight" title="Hire freelance web designer Justin Chick" href="<?= esc_url(home_url('hire-justin')); ?>">You can hire me.</a></h2>
 				<p>I believe in simplicity and that the web should be easy to use for everyone.</p>
 			</div>
-			<div class="col-sm-6 col-xs-10 first-xs last-sm last-md last-lg">
+			<div class="col-sm-5 col-xs-10 first-xs last-sm last-md last-lg">
 				<img src="<?php echo get_template_directory_uri() . '/dist/images/hat.jpg'; ?>">
 			</div>
 		</div>
 
-		<div class="row center-xs mt4 mb4">
-			<div class="col-xs-12">
-				<h2 class="m0 text-center">Projects I've Been A Part Of</h2>
+		<div class="row middle-xs border-top-gray border-bottom-gray pt3 mt4 pb3 mb4">
+			<div class="col-sm-6 p0">
+				<a href="#" class="launch-card middle-xs p2 flex">
+					<?php get_template_part( 'templates/imac' ); ?>
+					<div class="col-xs">
+						<div class="card-text">
+							<h2 data-heading>Work Portfolio</h2>
+							<p class="m0">Selected collection of client projects</p>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-sm-6 p0">
+				<a href="#" class="launch-card middle-xs p2 flex">
+					<?php get_template_part( 'templates/launches', 'launching' ); ?>
+					<div class="col-xs">
+						<div class="card-text">
+							<h2 data-heading>12 Launches</h2>
+							<p class="m0">1 project per month for 2016</p>
+						</div>
+					</div>
+				</a>
 			</div>
 		</div>
 		<div class="row center-xs">
@@ -45,13 +64,17 @@
 			<?php endwhile; endif; wp_reset_postdata(); ?>
 		</div>
 
+	</div>
+
+	<div class="container-fluid wrap">
+
 		
 		<div class="row center-xs mt4">
-			<div class="col-sm-10 col-xs-12">
+			<div class="col-sm-11 col-xs-12">
 				<p>Yes, the web should be easy to use for <em>everyone</em>.</p>
 				<p>It's people of all walks of life who use the web every day, and I keep this at the forefront of every decision I make. How will this feature/design/experience affect the people who'll be using this site? Who are those people and what do we know about them?</p>
 				<p>My work is driven by collaboration, an inquisitive nature, and some good ol’ fashioned elbow grease. With these three pillars (and over five years full-time freelance experience), I'm able to create thoughtful, holistic design solutions that get out of the way to allow people to accomplish what they need to.</p>
-				<p>I’m also a devout cyclist, <a href="<?= esc_url(home_url('category/photography/')); ?>" title="Photography of Justin Chick | @jchck_">amateur photographer</a>, social justice advocate, vegetarian, and avid DC Comics fan (Detective Comics 27, anyone?). Right now you can <a href="<?= esc_url(home_url('follow-along')); ?>" target="_blank">find me traveling around the globe</a>, reading under a tree or enjoying the day in a new cafe.</p>
+				<p>I’m also a devout cyclist, <a href="<?= esc_url(home_url('category/photography/')); ?>" title="Photography of Justin Chick | @jchck_">amateur photographer</a>, social justice advocate, vegetarian, and avid DC Comics fan (Detective Comics 27, anyone?). Right now you can find me rolling around the western half of North America by train, reading under a tree or enjoying the day in a new cafe.</p>
 				<p>Want to find out what we can do together? <a title="Hire Justin Chick freelance web designer" class="highlight" href="<?= esc_url(home_url('hire-justin')); ?>">This is a good first step.</a></p>
 			</div>
 		</div>
