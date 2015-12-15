4e717853f7f23b25ad72df74a223d377bfe68212
jchck <hey@justinchick.com>
2015-12-15 16:00:39 -0500

    add loop args

--- archive-12-launches.php
+++ archive-12-launches.php
@@ -1,10 +1,28 @@
-<div class="container-fluid wrap-12-launches">
+<?php
+    $args = array(
+		//Type & Status Parameters
+		'post_type'   => '12-launches',
+		'post_status' => 'publish',
+		
+		//Order & Orderby Parameters
+		'order'               => 'DESC',
+		'orderby'             => 'date',
+		
+		//Pagination Parameters
+		'posts_per_page'         => 12
+			
+		);
+
+    $query = new WP_Query( $args );
+?>
+
+<div class="container-fluid wrap">
 	<h1 data-heading>12 Launches</h1>
 	<p class="mt4 mb4 pb4 border-bottom-gray">Climb leg rub face on everything give attitude nap all day for under the bed. Chase mice attack feet but rub face on everything hopped up on goofballs. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas. Dramatically maintain clicks-and-mortar.</p>
 	<div class="row">
 		<!-- Static Row 1 -->
 		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
-			<a href="#" class="launch-card flex middle-xs p2">
+			<a href="<?= esc_url(home_url('hire-justin')); ?>" class="launch-card flex middle-xs p2">
 				<?php get_template_part( 'templates/launches', 'jframe' ); ?>
 				<div class="col-xs">
 					<div class="card-text ml2 mr2">
