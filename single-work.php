93e912a0d5ed355a67f6ec2b2cef725a6bf9e612
jchck <hey@justinchick.com>
2015-12-15 15:43:25 -0500

    single portfolio project link

--- single-work.php
+++ single-work.php
@@ -2,19 +2,17 @@
 	<?php while (have_posts()) : the_post(); ?>
 		<?php get_template_part('templates/page', 'header'); ?>
 		<div class="row center-xs">
-			<a class="project-svg mb2" href="#">
-				<?php if (get_field('project_svg_logo')) {
-					the_field('project_svg_logo');
-				} else {
-					get_template_part( 'templates/launches', 'jframe' );
-				} ?>
-				<p>View Project</p>
-			</a>
+			<?php if( get_field( "project_url" ) ): ?>
+				<a class="project-svg" href="<?php the_field('project_url'); ?>">
+					<?php if (get_field('project_svg_logo')) {
+						the_field('project_svg_logo');
+					} else {
+						get_template_part( 'templates/launches', 'jframe' );
+					} ?>
+					<p>View Project</p>
+				</a>
+			<?php endif; ?>
 		</div>
 		<?php get_template_part('templates/content', 'page'); ?>
-		<?php if( get_field( "project_url" ) ): ?>
-			<p class="mb3 text-center"><a target="_blank" href="<?php the_field('project_url'); ?>" class="btn btn-blue">View Project</a></p>
-		<?php endif; ?>
-		
 	<?php endwhile; ?>
 </div>
