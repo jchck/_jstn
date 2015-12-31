a0fdc439180d4d609c8f10dba3dc6d2e87a300f4
jchck <hey@justinchick.com>
2015-12-31 11:47:14 -0500

    config.php -> setup.php

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ $sage_includes = [
   'lib/init.php',                  // Initial theme setup and constants
   'lib/wrapper.php',               // Theme wrapper class
   'lib/conditional-tag-check.php', // ConditionalTagCheck class
-  'lib/config.php',                // Configuration
+  'lib/setup.php',                 // Theme setup (previously config.php)
   'lib/assets.php',                // Scripts and stylesheets
   'lib/titles.php',                // Page titles
   'lib/extras.php',                // Custom functions
