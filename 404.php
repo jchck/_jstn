a545b9080aaf5464da41b459a4e2c9250acab49f
jchck <hey@justinchick.com>
2015-12-31 12:21:44 -0500

    styling 404 w use of new form

--- 404.php
+++ 404.php
@@ -8,15 +8,7 @@
 	</div>
 	<div class="col-sm-6 col-xs-9">
 		<p class="ml2-sm">Are you as confused as this kid right now?</p>
-		<p class="ml2-sm">Don't worry about it, not everything lines up the way you think it should.</p>
-		<p class="ml2-sm"><a href="<?= esc_url(home_url('/ck')); ?>">It'll be alright</a></p>
-	</div>
-</div>
-
-<div class="row">
-	<div class="col-sm">
-		<div class="m4 text-center">
-			<?php get_search_form(); ?>
-		</div>
+		<p class="ml2-sm">It'll be alright, maybe this search form can help?</p>
+		<p class="ml2-sm"><?php get_search_form(); ?></p>
 	</div>
 </div>
