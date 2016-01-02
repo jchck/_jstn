bd9564e60f93d113cabbe45e26461960dace63b2
jchck <hey@justinchick.com>
2016-01-02 12:33:18 -0500

    hide banner on xs

--- templates/banner.php
+++ templates/banner.php
@@ -1,3 +1,3 @@
-<div id="banner" class="slider">
+<div id="banner" class="slider hidden-xs">
 	<a href="<?= esc_url(home_url('follow-along')); ?>" class='pt2 pb2'>You should join my mailing list, I think you'll really enjoy it</a>
 </div>
