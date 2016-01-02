c59804835bfb234ad0b2a3ed7fac2ee5650c778b
jchck <hey@justinchick.com>
2016-01-02 11:32:40 -0500

    front page copy edit

--- front-page.php
+++ front-page.php
@@ -44,7 +44,7 @@
 		<div class="col-sm-10 col-xs-12">
 			<div class="bg-off-white border-radius p4">
 				<p>Yes, the web should be easy to use for <em>everyone</em>.</p>
-				<p>It's people of all walks of life who use the web every day, and I keep this at the forefront of every decision I make. How will this feature/design/experience affect the people who'll be using this site? Who are those people and what do we know about them?</p>
+				<p>It's people of all walks of life who use the web every day, and I keep this at the forefront of every decision I make. How will this feature, design, experience affect the people who'll be using this site? Who are those people and what do we know about them?</p>
 				<p>My work is driven by collaboration, an inquisitive nature, and some good ol’ fashioned elbow grease. With these three pillars (and over five years full-time freelance experience), I'm able to create thoughtful, holistic design solutions that get out of the way to allow people to accomplish what they need to.</p>
 				<p>I’m also a devout cyclist, <a href="<?= esc_url(home_url('category/photography/')); ?>" title="Photography of Justin Chick | @jchck_">amateur photographer</a>, social justice advocate, vegetarian, and avid DC Comics fan (Detective Comics 27, anyone?). Right now you can <a href="<?= esc_url(home_url('/follow-along')); ?>" tltle="">follow me treking around the globe</a>, reading under a tree, or enjoying the day in a new cafe.</p>
 				<p class="m0">Want to find out what we can do together? <a title="Hire Justin Chick freelance web designer" class="highlight" href="<?= esc_url(home_url('hire-justin')); ?>">This is a good first step.</a></p>
