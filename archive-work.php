2c2b43f1a2b3a43115f325804f6eb40b560f99b4
jchck <hey@justinchick.com>
2016-01-06 17:55:30 -0500

    responsiveness for archive-work.php

--- archive-work.php
+++ archive-work.php
@@ -22,15 +22,17 @@
 		<?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
 
 		<article <?php post_class('col-md-7 col-sm-10 col-xs-12 flex p0'); ?>>
-			<a href="<?php the_permalink(); ?>" class="color-box color-targeting flex middle-xs p3">
-				<?php if (get_field('project_svg_logo')) {
-					the_field('project_svg_logo');
-				} else {
-					get_template_part( 'templates/launches', 'jframe' );
-				} ?>
-				<div class="width-100 ml2 mr2">
-					<h3><?php the_title(); ?></h3>
-					<p class="m0"><?php the_field('project_summary') ?></p>
+			<a href="<?php the_permalink(); ?>" class="color-box color-targeting row middle-xs center-xs p3">
+				<div class="col-md col-sm col-xs-4">
+					<?php if (get_field('project_svg_logo')) {
+						the_field('project_svg_logo');
+					} else {
+						get_template_part( 'templates/launches', 'jframe' );
+					} ?>
+				</div>
+				<div class="col-md-10 col-sm-9 col-xs-12">
+					<h3 class="mt0 ml1-md"><?php the_title(); ?></h3>
+					<p class="m0 ml1-md"><?php the_field('project_summary') ?></p>
 				</div>
 			</a>
 		</article>
