8648efc8e432e1be117f83141630b2cab3ff8fea
jchck <hey@justinchick.com>
2016-01-22 14:56:43 +0200

    sign up CTA

--- templates/banner.php
+++ templates/banner.php
@@ -1,3 +1,3 @@
 <div id="banner" class="banner hidden-xs">
-	<a href="<?= esc_url(home_url('follow-along')); ?>" class='pt2 pb2'>You should join my mailing list, I think you'll really enjoy it</a>
+	<a href="<?= esc_url(home_url('follow-along')); ?>" class='pt2 pb2'>Want me send you a letter every week? Click here.</a>
 </div>
