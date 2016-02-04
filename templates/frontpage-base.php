7a6be4b72dc7c9b6b1dcb787504c172a42860090
jchck <hey@justinchick.com>
2016-02-04 12:28:36 +0200

    implement new text link styles on front-page

--- templates/frontpage-base.php
+++ templates/frontpage-base.php
@@ -1,6 +1,6 @@
 <div class="container-fluid wrap">
 
-	<div class="row middle-xs center-xs between-sm">
+	<div class="row middle-xs center-xs between-sm link">
 		<div class="col-sm-6 col-xs-12">
 			<h1 class="lead mt2 mb0 m0-xs">Hi, I'm Justin Chick.<br />I'm a front-end developer located in Cape Town, South Africa.</h1>
 			<h2 class="lead mb2-sm mb3-md mt0"><a class="highlight" title="Hire freelance web designer Justin Chick" href="<?= esc_url(home_url('hire-justin')); ?>">You can hire me.</a></h2>
@@ -42,7 +42,7 @@
 			
 	<div class="row center-xs mt4">
 		<div class="col-sm-10 col-xs-12">
-			<div class="bg-off-white border-radius p1 p4-xs">
+			<div class="bg-off-white border-radius p1 p4-xs link">
 				<?php get_template_part( 'templates/frontpage', 'copy' ); ?>
 			</div>
 		</div>
--- templates/frontpage-copy.php
+++ templates/frontpage-copy.php
@@ -4,6 +4,6 @@
 
 <p>My work is driven by collaboration, an inquisitive nature, and some good ol’ fashioned elbow grease. With these three pillars (and over five years full-time freelance experience), I'm able to create thoughtful, holistic design solutions that get out of the way to allow people to accomplish what they need to.</p>
 
-<p>I’m also a devout cyclist, <a class="new" href="<?= esc_url(home_url('category/photography/')); ?>" title="Photography of Justin Chick | @jchck_">amateur photographer</a>, social justice advocate, vegetarian, and avid DC Comics fan (Detective Comics 27, anyone?). Right now you can <a class="new" href="<?= esc_url(home_url('/follow-along')); ?>" tltle="">follow me treking around the globe</a>, reading under a tree, or enjoying the day in a new cafe.</p>
+<p>I’m also a devout cyclist, <a class="link" href="<?= esc_url(home_url('category/photography/')); ?>" title="Photography of Justin Chick | @jchck_">amateur photographer</a>, social justice advocate, vegetarian, and avid DC Comics fan (Detective Comics 27, anyone?). Right now you can <a class="link" href="<?= esc_url(home_url('/follow-along')); ?>" tltle="">follow me treking around the globe</a>, reading under a tree, or enjoying the day in a new cafe.</p>
 
-<p class="m0">Want to find out what we can do together? <br /> <a title="Hire Justin Chick freelance web designer" class="new" href="<?= esc_url(home_url('hire-justin')); ?>">This is a good first step.</a></p>
+<p class="m0">Want to find out what we can do together? <br /> <a title="Hire Justin Chick freelance web designer" class="link" href="<?= esc_url(home_url('hire-justin')); ?>">This is a good first step.</a></p>
