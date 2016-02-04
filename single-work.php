3915d0679f03bfed77f77446e96def34b07769b9
jchck <hey@justinchick.com>
2016-02-04 13:38:57 +0200

    add work pagnation

--- single-work.php
+++ single-work.php
@@ -15,4 +15,17 @@
 		</div>
 		<?php get_template_part('templates/content', 'page'); ?>
 	<?php endwhile; ?>
+	<div class="clearfix">
+		<?php if ( get_adjacent_post( false, '', true ) ): ?>
+			<span class="btn btn-blue btn-sm pull-left">
+				<?php previous_post_link('%link', 'Previous'); ?>
+			</span>
+		<?php endif; ?>
+
+		<?php if ( get_adjacent_post( false, '', false ) ): ?>
+			<span class="btn btn-blue btn-sm pull-right">
+				<?php next_post_link('%link', 'Next'); ?>
+			</span>
+		<?php endif; ?>
+	</div>
 </div>
