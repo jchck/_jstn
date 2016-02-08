0060bccd45c3cc1307fb2acf3e80b72fa07467a1
jchck <hey@justinchick.com>
2016-02-08 21:42:18 +0200

    add jFrame to 12-launches archive

--- archive-12-launches.php
+++ archive-12-launches.php
@@ -5,12 +5,12 @@
 	<div class="row">
 		<!-- Static Row 1 -->
 		<article <?php post_class('col-md-4 col-sm-6 col-xs-12 flex p0'); ?>>
-			<a href="<?= esc_url(home_url('12-launches/')); ?>" class="color-box flex middle-xs p2">
+			<a href="http://jchck.github.io/jframe/" target="_blank" class="color-box flex middle-xs p2">
 				<?php get_template_part( 'templates/launches', 'launching' ); ?>
 				<div class="col-xs">
 					<div class="width-100 ml2 mr2">
-						<h3 class="mt4 mb2">Coming Soon</h3>
-						<p class="m0">Launching January</p>
+						<h3 class="mt4 mb2">jFrame.css</h3>
+						<p class="m0">Launched 31 Jan</p>
 					</div>
 				</div>
 			</a>
