2d5b7a466afd34492517e5d300ac839c9f3d9bbe
jchck <hey@justinchick.com>
2016-03-15 12:48:19 +0200

    a bit more white space

--- templates/frontpage-base.php
+++ templates/frontpage-base.php
@@ -1,6 +1,6 @@
 <div class="container-fluid wrap frontpage-base">
 
-	<div class="row middle-xs center-xs between-sm link">
+	<div class="row middle-xs center-xs between-sm link pb3">
 		<div class="col-sm-6 col-xs-12">
 			<h1 class="lead mt2 mb0 m0-xs mb2-sm mb3-md">Hi, I'm Justin Chick.<br />I'm a digital nomad and freelance front-end developer.</h1>
 			<p class="m0">I live out of a backpack, take risks every day, and help other freelancers do the same.</p>
@@ -10,7 +10,7 @@
 		</div>
 	</div>
 
-	<div class="border-top-gray border-bottom-gray pt3 mt4 pb3 mb4">
+	<div class="border-top-gray border-bottom-gray pt4 mt4 pb4 mb4">
 
 		<h3 class="text-center m0">Signup to get inspired every week.</h3>
 		
