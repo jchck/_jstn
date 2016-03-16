4a9f7618e8fc86c7762c442a50dfb2ea81e70db7
jchck <hey@justinchick.com>
2016-03-16 17:00:02 +0200

    styling/cleaning up

--- templates/ouibounce.php
+++ templates/ouibounce.php
@@ -2,14 +2,12 @@
   <div class="underlay"></div>
   <div class="modal">
     <div class="modal-title">
-      <h3>This is a Ouibounce modal</h3>
+      <h3 class="text-center">This is a Ouibounce modal</h3>
     </div>
 
     <div class="modal-body">
       <p>Thanks for stoping by!</p>
-      <br>
       <p>You can style your Ouibounce modal however you see fit. You can also include big buttons, an email capture or just plain text. The sky's the limit!</p>
-      <br>
       <p>Close this modal by clicking "No Thanks" below or clicking anywhere outside of the modal.</p>
 
       <form>
