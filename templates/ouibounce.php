9486642af3c3e92efff226287fa4f9695889365c
jchck <hey@justinchick.com>
2016-03-17 11:45:09 +0200

    v bump [deploy: production]

--- templates/ouibounce.php
+++ templates/ouibounce.php
@@ -9,8 +9,8 @@
       <p>Join the hundreds of other creatives on a path to living an amazing life.</p>
       <p>Signup and I'll deliver my best writing to your inbox every week before anyone else.</p>
 
-      <?php echo do_shortcode( '[gravityform id="4" title="false" description="false"]' ); ?>
-      <?php // echo do_shortcode( '[gravityform id="7" title="false" description="false"]' ); ?>
+      <?php// echo do_shortcode( '[gravityform id="4" title="false" description="false"]' ); ?>
+      <?php echo do_shortcode( '[gravityform id="7" title="false" description="false"]' ); ?>
     </div>
 
     <div class="modal-footer">
