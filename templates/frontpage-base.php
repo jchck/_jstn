a81c32b58d7bfdd53c3032c3474a5a36ce429a25
jchck <hey@justinchick.com>
2016-04-04 17:17:55 -0400

    typo! [deploy: production]

--- templates/frontpage-base.php
+++ templates/frontpage-base.php
@@ -3,7 +3,7 @@
 	<div class="row middle-xs center-xs between-sm link pb3">
 		<div class="col-sm-6 col-xs-12">
 			<h1 class="lead mt2 mb0 m0-xs mb2-sm mb3-md">Hi, I'm Justin Chick.</h1>
-			<p class="lead m0">I create custom WordPress sites for entreprenures who want to grow their business.</p>
+			<p class="lead m0">I create custom WordPress sites for entrepreneurs who want to grow their business.</p>
 		</div>
 		<div class="col-sm-5 col-xs-12 first-xs last-sm last-md last-lg">
 			<img style="width: 100%" title="Freelance WordPress Web Designer Justin Chick" src="<?php echo get_template_directory_uri() . '/dist/images/hat.jpg'; ?>">
