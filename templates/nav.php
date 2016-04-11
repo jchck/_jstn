b5df9b65a3060d7ee5bcf08a39bedc78607ae200
jchck <hey@justinchick.com>
2016-04-11 10:57:05 -0400

    remove newsletter CTA from <nav>

--- templates/nav.php
+++ templates/nav.php
@@ -5,5 +5,4 @@
 	  wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav mt2']);
 	endif;
 	?>
-	<p class="m0 mt3 text-center border-top hidden-xs"><a href="<?= esc_url(home_url('follow-along')); ?>" class="btn btn-blue mt3 mb3">Get my Newsletter!</a></p>
 </nav>
