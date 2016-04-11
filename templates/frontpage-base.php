2822ef6f42a8aef37cd2596ec043cc8d6b44cdb0
jchck <hey@justinchick.com>
2016-04-11 15:43:17 -0400

    locking down that new frontpage

--- templates/frontpage-base.php
+++ templates/frontpage-base.php
@@ -1,10 +1,10 @@
-<div class="hero pt4-sm pb4-sm">
+<section class="hero pt4-sm pb4-sm">
 	<div class="wrap center-block">
 		<h1 class="text-center m0">I build creative platforms to support your business goals.</h1>
 	</div>
-</div>
+</section>
 
-<div class="wrap-sm center-block frontpage-base">
+<section class="wrap-sm center-block frontpage-base">
 	<h2 class="text-center">You're an entrepreneur, small business owner, or tech start-up...</h2>
 	<p>You’ve got skills and expertise to spare, but you’re so busy running your business that your website is a little dusty. Or maybe there’s no site at all. You’re MIA online.</p>
 
@@ -17,45 +17,38 @@
 	<h3 class="text-center">That's why I do what I do.</h3>
 	<p>Vestibulum vitae velit eu tortor fermentum accumsan tempor at tellus. Duis at fringilla odio. Curabitur ut quam eu mauris egestas malesuada a at nunc. Etiam ac auctor nisl. Phasellus id enim justo. Nulla quis ligula lobortis, convallis dui id, dictum mauris. Cras ultrices pharetra tortor, non dignissim massa. Duis vitae vehicula ipsum. Aenean molestie ex in justo placerat gravida. Etiam interdum eros leo, a tempus mauris ultrices ac. Maecenas elit odio, vestibulum id commodo ut, sagittis et massa.</p>
 	<p class="text-center"><a href="#" class="btn btn-blue">Let's Work Together</a></p>
-</div>
+</section>
 
-<div class="stragety pt4-sm pb4-sm">
+<section class="stragety pt4-sm pb4-sm">
 	<div class="container-fluid wrap">
-		<div class="row">
-			<div class="col-sm-4">
+		<div class="row center-xs">
+			<div class="col-xs-10 col-sm-4">
 				<h4 class="text-center mt0">Level Up Your Website</h4>
 				<p class="m0">Get a website that delivers results. In other words, you'll see lower bounce rates, more inqueries, and higher sales. These are measurable changes that smart content, design, and stragety will support.</p>
 			</div>
-			<div class="col-sm-4">
+			<div class="col-xs-10 col-sm-4">
 				<h4 class="text-center mt0">Simple, One Stop Service</h4>
 				<p class="m0">Once I understand who you are and where you're going, I'll take you through the entire process. That means custom design, snappy content, and full intergration. It's fast, seamless, and it's the full package (with no shortcuts).</p>
 			</div>
-			<div class="col-sm-4">
+			<div class="col-xs-10 col-sm-4">
 				<h4 class="text-center mt0">Creativity + Experience</h4>
 				<p class="m0">I've got a long list of happy clients and over 6 years of experience. I want this process to feel simple and, dare I say, fun? I believe in clear communication, great relationships, and a web platform that works for you and customers.</p>
 			</div>
 		</div>
 	</div>
-</div>
+</section>
 
-<div class="container-fluid wrap frontpage-base">
-
-	<div class="row middle-xs center-xs between-sm link pb3">
-		<div class="col-sm-6 col-xs-12">
-			<h1 class="lead mt2 mb0 m0-xs mb2-sm mb3-md">Hi, I'm Justin Chick.</h1>
-			<p class="lead m0">I create custom WordPress sites for entrepreneurs who want to grow their business.</p>
-		</div>
-		<div class="col-sm-5 col-xs-12 first-xs last-sm last-md last-lg">
-			<img style="width: 100%" title="Freelance WordPress Web Designer Justin Chick" src="<?php echo get_template_directory_uri() . '/dist/images/hat.jpg'; ?>">
+<section class="bio pt4-sm pb4-sm">
+	<div class="container-fluid wrap-sm">
+		<div class="row center-xs middle-xs">
+			<div class="col-xs-10 col-sm-6">
+				<h3 class="text-center">Justin Chick</h3>
+				<p>Quisque non lacus mi. Sed vulputate lorem sed nisi maximus porta. Nulla lacinia est non ante varius commodo. Suspendisse sodales tristique volutpat. Maecenas lacinia urna enim, in tincidunt ex vehicula sed. Duis eu maximus leo, vel pretium ipsum.</p>
+			</div>
+			<div class="col-xs-10 col-sm-6">
+				<img title="Freelance WordPress Web Designer Justin Chick" src="<?php echo get_template_directory_uri() . '/dist/images/hat.jpg'; ?>">
+			</div>
 		</div>
 	</div>
+</section>
 
-	<div class="border-top-gray border-bottom-gray pt4 mt4 pb4 mb4">
-
-		<h3 class="text-center m0">Signup to get inspired every week.</h3>
-		
-		<?php echo do_shortcode( '[gravityform id="7" title="false" description="false"]' ); ?>
-
-	</div>
-
-</div>
