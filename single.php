dfc84efc850c3a0418a821ac915a54f238463382
jchck <hey@justinchick.com>
2016-04-13 16:10:34 -0400

    cleaning up single.php

--- single.php
+++ single.php
@@ -1,1 +1,3 @@
-<?php get_template_part('templates/content-single', get_post_type()); ?>
+<div class="wrap-sm container-fluid">
+	<?php get_template_part('templates/content-single', get_post_type()); ?>
+</div>
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,22 +1,12 @@
-<?php use Roots\Sage\Category; ?>
 <?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
+  <article <?php post_class('mt3'); ?>>
     <header class="text-center">
-      <h1><?php the_title(); ?></h1>
-      
-        <div class="center-block">
-          <div class="position-relative post-card circle img-size flex flex-center">
-            <div style="background: url('<?php the_field('main_image'); ?>'); background-size: cover;"></div>
-          </div>
-        </div>
-
+      <h1 class="mt0"><?php the_title(); ?></h1>
     </header>
-    <div class="the-content link">
       <?php
         get_template_part( 'templates/video' );
         the_content();
       ?>
-    </div>
     <footer>
       <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
     </footer>
