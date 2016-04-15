fe096df2fb6f88618bb66fdc62277dfd01c06c62
jchck <hey@justinchick.com>
2016-04-14 21:39:54 -0400

    clean the heck out of our JS

--- lib/assets.php
+++ lib/assets.php
@@ -71,7 +71,5 @@ function assets() {
   wp_enqueue_script('ouibounce', '//cdnjs.cloudflare.com/ajax/libs/ouibounce/0.0.11/ouibounce.min.js', ['jquery'], null, true);
 
   wp_enqueue_script('sage/js', asset_path('scripts/main.js'), ['jquery'], null, true);
-
-  wp_enqueue_script( 'gmap', '//maps.google.com/maps/api/js', [], null, true );
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
