c6cd093040e978472d05eca0bcc6d99654309525
jchck <hey@justinchick.com>
2016-05-04 11:52:30 -0400

    add comments

--- templates/content.php
+++ templates/content.php
@@ -1,3 +1,13 @@
+<?php 
+/* 
+	Used in the_post(); loop
+	Served via index.php, rather than via home.php
+
+	When Reading Settings :
+		Front Page = null
+		Posts Page = Articles
+*/	
+?>
 <article <?php post_class('col-xs-12 pb4'); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
