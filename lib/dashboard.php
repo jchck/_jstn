017d78d93755c846bb9f284bf3b1c20f4e35318c
jchck <hey@justinchick.com>
2016-05-04 17:25:17 -0400

    all lorem ipsum & such

--- lib/dashboard.php
+++ lib/dashboard.php
@@ -28,6 +28,12 @@ function jchck_widget(){
 		<div id="jchck-widget" class="welcome-panel" style="display:none">
 			<div class="welcome-panel-content">
 				<h2 style="text-align: center">sup?</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
 		</div>
 
