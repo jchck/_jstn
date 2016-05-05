3de13add3bf6c9b5b26298b4cb977daf50c475a2
jchck <hey@justinchick.com>
2016-05-04 20:30:41 -0400

    remove front-page redirect

--- front-page.php
+++ front-page.php
@@ -1,4 +1,5 @@
 <?php 
-wp_safe_redirect( esc_url( home_url( '/ck' ) ), 301 );
-exit;
+// wp_safe_redirect( esc_url( home_url( '/ck' ) ), 301 );
+// exit;
+get_template_part( 'templates/frontpage', 'base' );
 ?>
