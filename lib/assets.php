a8819a25d4353cb9a384a0d171f63f60990c356c
jchck <hey@justinchick.com>
2016-05-06 00:33:56 -0400

    ensue the processed css file

--- lib/assets.php
+++ lib/assets.php
@@ -3,6 +3,6 @@
 namespace Jchck\Assets;
 
 function assets(){
-	wp_enqueue_style('css', get_template_directory_uri() . '/dest/styles.css', false, null);
+	wp_enqueue_style('css', get_template_directory_uri() . '/dest/jchck_.css', false, null);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
