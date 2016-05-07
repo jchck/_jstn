8bc05853981da02cb50d649de1dc32197c6d1080
jchck <hey@justinchick.com>
2016-05-07 17:26:59 -0400

    add tagline & nav to header

--- templates/header.php
+++ templates/header.php
@@ -1,9 +1,18 @@
 <header class="lg-py4">
 	<div class="flex flex-wrap items-center">
 		<div>
-			<a class="" href="<?= esc_url(home_url('/')); ?>">
+			<a href="<?= esc_url(home_url('/')); ?>">
 				<?php get_template_part( 'templates/logo', 'jc' ); ?>
 			</a>
+			<p>I build creative platforms to meet your business needs</p>
+		</div>
+		<div class="flex-auto"></div>
+		<div>
+			<ul class="list-reset">
+				<li class="inline-block mr1">Articles</li>
+				<li class="inline-block mr1">Tutorials</li>
+				<li class="inline-block mr1">Work</li>
+			</ul>
 		</div>
 	</div>
 </header>
