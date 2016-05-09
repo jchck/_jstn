92fb2b101fcc3820b4282fc5931d5691fb07a837
jchck <hey@justinchick.com>
2016-05-09 16:45:42 -0400

    add to the comment & clean up code

--- lib/setup.php
+++ lib/setup.php
@@ -51,11 +51,12 @@ add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
 /**
  *
  * Add class' into nav menu <li>'s
+ * @see https://codex.wordpress.org/Plugin_API/Filter_Reference/nav_menu_css_class
  *
  */
-function nav_menu($class, $items, $args){
+function nav_menu($class){
 	$class[] = 'inline-block mr1';
 	return $class;
 }
 
-add_filter('nav_menu_css_class', __NAMESPACE__ . '\\nav_menu', 1, 3);
+add_filter('nav_menu_css_class', __NAMESPACE__ . '\\nav_menu');
