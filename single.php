ee03623b00e7a67a967e45dde7355544283bbfed
jchck <hey@justinchick.com>
2016-05-12 15:05:15 -0400

    create and begin styling single template

---
+++ single.php
@@ -0,0 +1,10 @@
+<?php while ( have_posts() ): the_post(); ?>
+	<div class="clearfix">
+		<header class="col sm-col-6 sm-pr2">
+			<?php get_template_part( 'templates/page', 'title' ); ?>
+		</header>
+		<article class="col sm-col-6">
+			<?php get_template_part( 'templates/content', 'page' ); ?>
+		</article>
+	</div>
+<?php endwhile; ?>
