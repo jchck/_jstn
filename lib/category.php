a7a29e5a4281e09ce92b64d8364f6cbaefd09cc4
jchck <hey@justinchick.com>
2016-05-12 17:11:35 -0400

    expand the function to see if ! have category post OR uncat

--- lib/category.php
+++ lib/category.php
@@ -3,9 +3,16 @@
 namespace Jchck\Category;
 
 function categories(){
-	$categories = get_the_category();
-	foreach ($categories as $category) {
-		$link = get_category_link( $category );
-		echo '<li class="inline-block mr1"><a href="' . $link . '">' . $category->name . '</a></li>';
+	$array = [
+		'post',
+		'uncategorized'
+	];
+	
+	if(!has_category($array)){
+		$categories = get_the_category();
+		foreach ($categories as $category) {
+			$link = get_category_link( $category );
+			echo '<li class="inline-block mr1"><a href="' . $link . '">' . $category->name . '</a></li>';
+		}
 	}
 }
