8b1efd5f1a8d409c660b5fa8435a4a960cc38703
jchck <hey@justinchick.com>
2016-05-11 22:00:31 -0400

    refractor so last menu item doesn't have trailing margins

--- lib/menu.php
+++ lib/menu.php
@@ -8,9 +8,13 @@ namespace Jchck\Menu;
  * @see https://codex.wordpress.org/Plugin_API/Filter_Reference/nav_menu_css_class
  *
  */
-function nav_menu($class){
-	$class[] = 'inline-block mr1';
-	return $class;
+function nav_class($classes, $item){
+	if ($item->title == 'Articles' || $item->title == 'Tutorials') {
+		$classes[] = 'inline-block mr1';
+	} else {
+		$classes[] = 'inline-block';
+	}
+	return $classes;
 }
 
-add_filter('nav_menu_css_class', __NAMESPACE__ . '\\nav_menu');
+add_filter('nav_menu_css_class', __NAMESPACE__ . '\\nav_class', 10, 2);
