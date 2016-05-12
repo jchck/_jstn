21f3d29ea42379de13c918e1900cf06301c6a7f3
jchck <hey@justinchick.com>
2016-05-12 15:39:48 -0400

    add published on date

--- single.php
+++ single.php
@@ -9,6 +9,9 @@ use Jchck\Excerpt;
 		<header class="col sm-col-6 sm-pr3">
 			<?php get_template_part( 'templates/page', 'title' ); ?>
 			<?= Excerpt\excerpt(); ?>
+			<time datetime="<?= get_post_time('c', true); ?>">
+				<?= get_the_date(); ?>
+			</time>
 		</header>
 		<article class="col sm-col-6">
 			<?php get_template_part( 'templates/content', 'page' ); ?>
