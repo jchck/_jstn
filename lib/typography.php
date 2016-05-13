6b7889a58d38a54102e5b08541b4b532778402fa
jchck <hey@justinchick.com>
2016-05-12 20:18:31 -0400

    create function to add measure class to p tags inside the_content()

--- lib/typography.php
+++ lib/typography.php
@@ -1,3 +1,18 @@
 <?php
 
 namespace Jchck\Typography;
+
+function measure($content){
+	/**
+	 *
+	 * Add's .measure to all <p>'s inside of the_content()
+	 * @see http://php.net/manual/en/function.preg-replace.php
+	 * @see https://developer.wordpress.org/reference/functions/the_content/
+	 * @see http://wordpress.stackexchange.com/a/51682
+	 *
+	 */
+	
+	return preg_replace('/<p([^>]+)?>/', '<p$1 class="measure">', $content, -1);
+}
+
+add_filter('the_content', __NAMESPACE__ . '\\measure');
