2ad325f4c0899cc1b0667f12dd0da675f5cf0c66
jchck <hey@justinchick.com>
2016-05-13 12:30:47 -0400

    add comments to category function

--- lib/category.php
+++ lib/category.php
@@ -3,12 +3,22 @@
 namespace Jchck\Category;
 
 function categories(){
-	$array = [
+	/**
+	 *
+	 * This checks if the current post has any attached categories
+	 * not including those defined in $not_these array and if passing,
+	 * loops thru and echo's the attached categories
+	 * @see https://developer.wordpress.org/reference/functions/get_the_category
+	 * @see https://codex.wordpress.org/Function_Reference/get_category_link
+	 *
+	 */
+	
+	$not_these = [
 		'post',
 		'uncategorized'
 	];
 	
-	if(!has_category($array)){
+	if(!has_category($not_these)){
 		$categories = get_the_category();
 
 		echo '<ul class="list-reset">';
@@ -17,7 +27,7 @@ function categories(){
 			$link = get_category_link( $category );
 			echo '<li class="inline-block mr1"><a href="' . $link . '">' . $category->name . '</a></li>';
 		}
-		
+
 		echo '</ul>';
 	}
 }
