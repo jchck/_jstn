7c1aae23a3885eb0de3b6422715376066b95a84a
jchck <hey@justinchick.com>
2016-05-14 15:32:48 -0400

    tempating index

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
-<?php get_template_part( 'templates/page', 'title' ); ?>
+<header class="sm-col-6 sm-pr3">
+	<?php get_template_part( 'templates/page', 'title' ); ?>
+</header>
 
-<?php if (!have_posts()) : ?>
-	<?php _e('Whoops! Nothing here.', 'jchck'); ?>
-	<?php get_search_form(); ?>
-<?php endif; ?>
+<article class="sm-col-6">
 
-<?php while (have_posts()) : the_post(); ?>
-	<?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
+	<?php if (!have_posts()) : ?>
+		<?php _e('Whoops! Nothing here.', 'jchck'); ?>
+		<?php get_search_form(); ?>
+	<?php endif; ?>
+
+	<?php while (have_posts()) : the_post(); ?>
+		<?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+	<?php endwhile; ?>
+
+</article>
 
 <?php the_posts_navigation(); ?>
