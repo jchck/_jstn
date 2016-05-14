97b8431c925c9f61692d505672957cd79ec05a89
jchck <hey@justinchick.com>
2016-05-13 22:23:32 -0400

    consistant spacing

--- lib/setup.php
+++ lib/setup.php
@@ -3,13 +3,13 @@
 namespace Jchck\Setup;
 
 function setup(){
+
 	/**
 	 *
 	 * Turn on features of Soil Plugin
 	 * @see https://roots.io/plugins/soil/
 	 *
 	 */
-	
 	add_theme_support( 'soil-clean-up' );
 	add_theme_support( 'soil-nav-walker' );
 	add_theme_support( 'soil-nice-search' );
@@ -22,7 +22,6 @@ function setup(){
 	 * @see http://codex.wordpress.org/Function_Reference/add_theme_support#Title_Tag
 	 *
 	 */
-
 	add_theme_support( 'title-tag' );
 
 	/**
@@ -31,7 +30,6 @@ function setup(){
 	 * @see http://codex.wordpress.org/Function_Reference/register_nav_menus
 	 *
 	 */
-	
 	register_nav_menus( [
 		'primary_nav'	=> __('Primary Navigation', 'jchck')
 	] );
@@ -42,7 +40,6 @@ function setup(){
 	 * @see http://codex.wordpress.org/Function_Reference/add_theme_support#HTML5
 	 *
 	 */
-
 	add_theme_support('html5', ['caption', 'comment-form', 'comment-list', 'gallery', 'search-form']);
 }
 
