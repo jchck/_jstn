b56afa6049fb2a4b02ee0c9972cf8bb50c584b16
jchck <hey@justinchick.com>
2016-05-15 16:04:40 -0400

    wrap 404 logo in <a>

--- 404.php
+++ 404.php
@@ -1,5 +1,7 @@
 <div style="min-height: 100vh" class="flex flex-column col-12 justify-center items-center">
-	<?php get_template_part( 'templates/logo', 'jc' ); ?>
+	<a href="<?= esc_url(home_url('/')); ?>">
+		<?php get_template_part( 'templates/logo', 'jc' ); ?>
+	</a>
 	<?php get_template_part( 'templates/page', 'title' ); ?>
 	<?php get_search_form(); ?>
 </div>
