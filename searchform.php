595525f05129942fb9592f352e4568656b940885
jchck <hey@justinchick.com>
2016-05-15 15:41:14 -0400

    remove boilerplate from search form.php

--- searchform.php
+++ searchform.php
@@ -7,25 +7,15 @@
 	 */
 ?>
 
-<!-- <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<label>
-		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-	</label>
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search =)', 'submit button' ) ?>" />
-</form> -->
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 
-<form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
 	<div class="relative mb3">
-		<input class="block h3 p1 border-bottom border-width-skinny" type="text" required>
+
+		<input class="block h3 p1 border-bottom border-width-skinny" type="search" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" required />
 		<span class="highlight absolute col-12 left-0"></span>
 		<span class="bar relative block"></span>
-		<label class="h4 absolute">Name</label>
+		<label class="h4 absolute">Search</label>
+
 	</div>
-	<div class="relative mb3">
-		<input class="block h3 p1 border-bottom border-width-skinny" type="text" required>
-		<span class="highlight absolute col-12 left-0"></span>
-		<span class="bar relative block"></span>
-		<label class="h4 absolute">Email</label>
-	</div>
+
 </form>
---
+++ templates/_form.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+ *
+ * Boilerplate for Material Design inspired input boxes
+ * @see https://scotch.io/tutorials/google-material-design-input-boxes-in-css3
+ *
+ */
+
+?>
+
+<form role="search" method="get">
+	<div class="relative mb3">
+		<input class="block h3 p1 border-bottom border-width-skinny" type="text" required>
+		<span class="highlight absolute col-12 left-0"></span>
+		<span class="bar relative block"></span>
+		<label class="h4 absolute">Name</label>
+	</div>
+	<div class="relative mb3">
+		<input class="block h3 p1 border-bottom border-width-skinny" type="text" required>
+		<span class="highlight absolute col-12 left-0"></span>
+		<span class="bar relative block"></span>
+		<label class="h4 absolute">Email</label>
+	</div>
+</form>
