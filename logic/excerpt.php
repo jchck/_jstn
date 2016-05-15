cefe4abee4457390294e103bb076b31f8b351902
jchck <hey@justinchick.com>
2016-05-15 12:55:09 -0400

    search results template

--- logic/excerpt.php
+++ logic/excerpt.php
@@ -41,7 +41,7 @@ function classes($excerpt){
 	 *
 	 */
 
-	if (is_home() || is_archive()) {
+	if (is_home() || is_archive() || is_search()) {
 		$classes = str_replace('<p', '<p class="h3 measure"', $excerpt);
 	} else {
 		$classes = str_replace('<p', '<p class="h2"', $excerpt);
