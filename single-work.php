d95629a2bf9f3b1f32ef192c2f635d21b9a6cf27
jchck <hey@justinchick.com>
2016-05-16 11:13:12 -0400

    responsiveness on content area

--- single-work.php
+++ single-work.php
@@ -15,7 +15,7 @@ use Jchck\Work;
 	<header class="col-12">
 		<?php get_template_part( 'templates/page', 'title' ); ?>
 	</header>
-	<article class="sm-col-6">
+	<article class="col-12 sm-col-8 md-col-6">
 		<?php get_template_part( 'templates/content', 'page' ); ?>
 	</article>
 <?php endwhile; ?>
