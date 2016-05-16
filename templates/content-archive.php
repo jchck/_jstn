a69f2db4bd804861d8beb83c53ddab496e5904b2
jchck <hey@justinchick.com>
2016-05-16 10:49:40 -0400

    update name

--- templates/content-archive.php
+++ templates/content-archive.php
@@ -7,7 +7,7 @@ use Jchck\Work;
 <article class="col-12">
 	<h2 class="<?= Titles\classes(); ?> border-top border-width-skinny"><a class="text-decoration-none" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-	<div class="flex items-center work-excerpt">
+	<div class="flex items-center logo-work">
 		<a href="<?php the_permalink(); ?>">
 			<?= Work\svg(); ?>
 		</a>
