cb254a411b2918b64ebf7671c792dd542976200b
jchck <hey@justinchick.com>
2016-05-16 17:13:52 -0400

    add all WP open source projects

--- front-page.php
+++ front-page.php
@@ -15,4 +15,37 @@
 		<h2 class="mt0 pt2 bold"><a href="#">Another Title</a></h2>
 		<p class="measure">The part of lowercase letters (such as k, b, and d) that ascends above the x-height of the other lowercase letters in a face.</p>
 	</article>
+
+	<div class="col-12 border-top border-width-skinny">
+		<p class="caps mb0">Open Source</p>
+	</div>
+
+	<article class="col-12 my4">
+		<p class="mb0 caps">WordPress</p>
+		<h2 class="mt0 pt2 bold"><a href="https://github.com/jchck/jchck_" target="_blank">jchck_</a></h2>
+		<p class="measure">My current site.</p>
+		<h2 class="mt0 pt2 bold"><a href="https://github.com/jchck/modern-wordpress" target="_blank">Modern WordPress</a></h2>
+		<p class="measure">It's 2016, let's WordPress like it. Part of an ongoing tutorial series.</p>
+		<h2 class="mt0 pt2 bold"><a href="https://github.com/jchck/wp-pesticide" target="_blank">WordPress + Pesticide</a></h2>
+		<p class="measure">Lorem ipsum and such, etc.</p>
+		<h2 class="mt0 pt2 bold"><a href="https://github.com/jchck/bow-tie" target="_blank">Bow Tie</a></h2>
+		<p class="measure">A WordPress theme that is so fresh and so clean.</p>
+		<h2 class="mt0 pt2 bold"><a href="https://github.com/jchck/sagebox" target="_blank">Sagebox</a></h2>
+		<p class="measure">Just the stuff needed to Sage + Flexboxgrid + have a happy day.</p>
+		<h2 class="mt0 pt2 bold"><a href="https://github.com/jchck/here-now" target="_blank">Here Now</a></h2>
+		<p class="measure">A plugin to use WordPress for managing/hosting/etc a podcast</p>
+		<h2 class="mt0 pt2 bold"><a href="https://github.com/jchck/jaechicks-site" target="_blank">Jaechick's Site</a></h2>
+		<p class="measure">My previous WordPress site built on Roots.</p>
+		<h2 class="mt0 pt2 bold"><a href="https://github.com/jchck/wp-bower-theme" target="_blank">WP Bower</a></h2>
+		<p class="measure">Just a simple theme to share using Bower in WordPress. Part of a tutorial.</p>
+		<h2 class="mt0 pt2 bold"><a href="https://github.com/jchck/wp-recursion" target="_blank">WP Recursion</a></h2>
+		<p class="measure">A theme for those times when you really gotta show a client that you really did do something.</p>
+		<h2 class="mt0 pt2 bold"><a href="https://github.com/jchck/lorem-oberst" target="_blank">Lorem Oberst</a></h2>
+		<p class="measure">A plugin for all your Conor Oberst dummy text needs.</p>
+		<h2 class="mt0 pt2 bold"><a href="https://github.com/jchck/tweet-it" target="_blank">Tweet It</a></h2>
+		<p class="measure">A plugin to make it easier for your readers to tweet your content.</p>
+		<h2 class="mt0 pt2 bold"><a href="https://github.com/jchck/wp-modern-business" target="_blank">Modern Business</a></h2>
+		<p class="measure">A Roots WordPress template.</p>
+
+	</article>
 <?php endwhile; ?>
