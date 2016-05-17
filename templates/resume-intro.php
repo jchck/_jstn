59217088ed8579e9d5c84f341a3293c0f43df093
jchck <hey@justinchick.com>
2016-05-17 13:32:34 -0400

    resume small device testing

--- templates/resume-intro.php
+++ templates/resume-intro.php
@@ -1,8 +1,8 @@
-<header class="col-12 sm-col-6 sm-pr3">
+<header class="col-12 sm-col-6 sm-pr3 mb2 sm-mb0">
 	<?php get_template_part( 'templates/page', 'title' ); ?>
 	<p class="h3 mb1">I'm currently looking for full-time work in New England.<br />If you're intrested in working together drop me a line:</p>
-	<p class="h3 mt0 mb1 caps"><a href="mailto:hey@justinchick.com">hey@justinchick.com</a></p>
-	<p class="m0"><a href="#" target="_blank">Download PDF Resume</a></p>
+	<p class="h3 sm-mt0 mb2 mb1 caps"><a href="mailto:hey@justinchick.com">hey@justinchick.com</a></p>
+	<p class="sm-m0"><a href="#" target="_blank">Download PDF Resume</a></p>
 </header>
 <article class="col-12 sm-col-6 pb4">
 	<p class="measure">I’m a front-end developer with over 6 years experience in the WordPress ecosystem.</p>
