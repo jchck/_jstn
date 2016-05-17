939c27529eb2e5fe914ad7f55b88845f6ceae712
jchck <hey@justinchick.com>
2016-05-17 13:23:27 -0400

    dat responsive doe

--- templates/content-archive.php
+++ templates/content-archive.php
@@ -8,11 +8,11 @@ use Jchck\Work;
 <article class="col-12">
 	<h2 class="<?= Titles\classes(); ?> border-top border-width-skinny"><a class="text-decoration-none" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-	<div class="flex items-center logo-work">
-		<a href="<?php the_permalink(); ?>">
+	<div class="sm-flex items-center logo-work">
+		<a class="block center" href="<?php the_permalink(); ?>">
 			<?= Work\svg(); ?>
 		</a>
-		<div class="flex-auto ml3">
+		<div class="flex-auto sm-ml3">
 			<?= Excerpt\excerpt(); ?>
 		</div>
 	</div>
