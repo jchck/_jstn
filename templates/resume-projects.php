0a17b29537cdff07c2977eee9ade185e98c36420
jchck <hey@justinchick.com>
2016-05-16 23:10:24 -0400

    resume case studies links

--- templates/resume-projects.php
+++ templates/resume-projects.php
@@ -3,16 +3,16 @@
 </div>
 <article class="col-12 sm-col-6 pb4">
 	<h3 class="h2 bold mt0">The Chapel Law Group</h3>
-	<p class="measure caps m0"><a href="#">Case Study</a></p>
+	<p class="measure caps m0"><a href="<?= esc_url(home_url('/case-studies/chapel-law-group')); ?>">Case Study</a></p>
 	<p class="measure mt0">It was an honor and a privilege to oversee the Chapel Law Group’s branding and identity process. With an identity in place, I then developed a WordPress site tailor fit to the firm’s exact needs.</p>
 	<h3 class="h2 bold">Wash U Med School</h3>
-	<p class="measure caps m0"><a href="#">Case Study</a></p>
+	<p class="measure caps m0"><a href="<?= esc_url(home_url('/case-studies/wash-u-med-school')); ?>">Case Study</a></p>
 	<p class="measure mt0">I had the privilege of revamping the site for the Office of Faculty Affairs for the nationally renowned medical school. Unfortunately, a majority of the heavy lifting of the project rests behind a secure login.</p>
 	<h3 class="h2 bold">Dashtaki Law Firm</h3>
-	<p class="measure caps m0"><a href="#">Case Study</a></p>
+	<p class="measure caps m0"><a href="<?= esc_url(home_url('/case-studies/dashtaki-law-firm')); ?>">Case Study</a></p>
 	<p class="measure mt0">Although the Dashtaki Law Firm is on of my oldest active clients the current version of the firm’s site launched August of 2015. I provided creative direction of the law firm’s brand identity and took the firm through two complete site builds.</p>
 	<h3 class="h2 bold">Our Collective</h3>
-	<p class="measure caps m0"><a href="#">Case Study</a></p>
+	<p class="measure caps m0"><a href="<?= esc_url(home_url('/case-studies/our-collective')); ?>">Case Study</a></p>
 	<p class="measure mt0">Though in operation for just under a year, I helped co-found Our Collective to share in the making of a kind, humble, and just view of interaction design.</p>
 
 </article>
