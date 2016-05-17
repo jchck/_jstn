4964eeb4b83bb89fd3d414f9135cab8c8fa74623
jchck <hey@justinchick.com>
2016-05-16 22:55:32 -0400

    404 text block color

--- searchform.php
+++ searchform.php
@@ -12,7 +12,7 @@
 
 	<div class="relative mb3">
 
-		<input class="block h3 p1 border-bottom border-width-skinny" type="search" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" required />
+		<input class="bg-white block h3 p1 border-bottom border-width-skinny" type="search" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" required />
 		<span class="highlight absolute col-12 left-0"></span>
 		<span class="bar relative block"></span>
 		<label class="h4 absolute">Search</label>
