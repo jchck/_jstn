b868f4b5189634c3d4847c6e59b6526f81d44924
jchck <hey@justinchick.com>
2016-05-18 09:12:44 -0400

    comment out download resume

--- templates/resume-intro.php
+++ templates/resume-intro.php
@@ -2,7 +2,7 @@
 	<?php get_template_part( 'templates/page', 'title' ); ?>
 	<p class="h3 mb1">I'm currently looking for full-time work in New England.<br />If you're intrested in working together drop me a line:</p>
 	<p class="h3 sm-mt0 mb2 mb1 caps"><a href="mailto:hey@justinchick.com">hey@justinchick.com</a></p>
-	<p class="sm-m0"><a href="#" target="_blank">Download PDF Resume</a></p>
+	<!-- <p class="sm-m0"><a href="#" target="_blank">Download PDF Resume</a></p> -->
 </header>
 <article class="col-12 sm-col-6 pb4">
 	<p class="measure">I’m a front-end developer with over 6 years experience in the WordPress ecosystem.</p>
