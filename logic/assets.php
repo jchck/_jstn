85bd7eddf3bf067c22fb734caed1cbb38bb9768b
jchck <hey@justinchick.com>
2016-05-20 13:07:21 -0400

    enqueue prism css when needed

--- logic/assets.php
+++ logic/assets.php
@@ -9,10 +9,18 @@ add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
 
 
 if (class_exists( 'acf' )) {
+
 	function prism_js(){
 		if (get_field('is_code')) {
 			$prism_js = locate_template( 'templates/prism-js.php' );
 			require_once $prism_js;
 		}
 	}
+
+	function prism_css(){
+		if (get_field('is_code')) {
+			wp_enqueue_style( 'prism_css', get_template_directory_uri() . '/dest/prism.css', false, null );
+		}
+	}
+	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\prism_css', 100 );
 }
