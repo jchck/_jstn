1f862cd6fdc10dd75f34fce3746340d2d7e8b44a
jchck <hey@justinchick.com>
2016-05-20 13:01:31 -0400

    rewrite with the class check first

--- logic/assets.php
+++ logic/assets.php
@@ -8,14 +8,11 @@ function assets(){
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
 
 
-
-function prism_js(){
-	if (class_exists( 'acf')) {
-		
+if (class_exists( 'acf' )) {
+	function prism_js(){
 		if (get_field('is_code')) {
 			$prism_js = locate_template( 'templates/prism-js.php' );
 			require_once $prism_js;
 		}
-
 	}
 }
