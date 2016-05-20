60606975a2bb0cd93d19c00b78654d2b63880ed6
jchck <hey@justinchick.com>
2016-05-19 21:42:55 -0400

    attach prism.js inline but not at end of page

--- logic/assets.php
+++ logic/assets.php
@@ -6,3 +6,18 @@ function assets(){
 	wp_enqueue_style('css', get_template_directory_uri() . '/dest/jchck_.css', false, null);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
+
+
+
+function code(){
+	if (class_exists( 'acf')) {
+		
+		if (get_field('is_code')) {
+			$prism = locate_template( 'templates/prism-js.php' );
+			//$prism = get_template_part( 'templates/prism', 'js' );
+			require_once $prism;
+		}
+
+	}
+	add_action( 'wp_footer', __NAMESPACE__ . '\\code' );
+}
