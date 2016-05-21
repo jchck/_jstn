f998c91341fecbcb69279f777fce47b3b6cfc807
jchck <hey@justinchick.com>
2016-05-21 10:25:30 -0400

    comments

--- logic/video.php
+++ logic/video.php
@@ -2,6 +2,15 @@
 
 namespace Jchck\Video;
 
+/**
+ *
+ * Responsive iFrame's
+ * Credit to Bootstrap
+ * @link http://getbootstrap.com/components/#responsive-embed
+ *
+ */
+
+
 function video(){
 	if (get_field('video')) {
 		echo '<div class="r-iframe pt0 px0 relative block overflow-hidden">';
