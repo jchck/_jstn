204004e33ab12b06ba54141ca411e67adb490c20
jchck <hey@justinchick.com>
2016-05-21 15:02:40 -0400

    WP_Query class and empty loop

--- templates/resume-projects.php
+++ templates/resume-projects.php
@@ -2,6 +2,28 @@
 	<h3 class="h1 border-top mt0 pt2 bold">Project Highlights</h3>
 </div>
 <article class="col-12 sm-col-6 pb4">
+
+	<?php 
+			/**
+			 * The WordPress Query class.
+			 * @link http://codex.wordpress.org/Function_Reference/WP_Query
+			 *
+			 */
+			$args = array(
+				//'category_name' => 'featured',
+				'post_type' => 'work',
+				'posts_per_page' => 4
+			);
+		
+		$query = new WP_Query( $args );
+		
+	 ?>
+
+	 <?php while( $query-have_posts() ) : $query->the_post(); ?>
+	 
+	<?php endwhile; ?>
+	<?php wp_reset_postdata(); ?>
+
 	<h3 class="h2 bold mt0">The Chapel Law Group</h3>
 	<p class="measure caps m0"><a href="<?= esc_url(home_url('/case-studies/chapel-law-group')); ?>">Case Study</a></p>
 	<p class="measure mt0">It was an honor and a privilege to oversee the Chapel Law Group’s branding and identity process. With an identity in place, I then developed a WordPress site tailor fit to the firm’s exact needs.</p>
