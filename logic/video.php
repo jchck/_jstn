6db2933e142c18c6b9a163f6e34ae631d208c551
jchck <hey@justinchick.com>
2016-05-21 10:22:56 -0400

    merry with basscss classes

--- logic/video.php
+++ logic/video.php
@@ -4,7 +4,7 @@ namespace Jchck\Video;
 
 function video(){
 	if (get_field('video')) {
-		echo '<div class="r-iframe">';
+		echo '<div class="r-iframe pt0 px0 relative block overflow-hidden">';
 			the_field('video');
 		echo '</div>';
 	}
