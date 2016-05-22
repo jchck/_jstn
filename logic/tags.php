ba77e0db74ef51bfe75f4e285363f5958bb01855
jchck <hey@justinchick.com>
2016-05-22 15:42:31 -0400

    loop tags

--- logic/tags.php
+++ logic/tags.php
@@ -2,3 +2,17 @@
 
 namespace Jchck\Tags;
 
+function tags(){
+	$tags = get_tags();
+
+	foreach ($tags as $tag) {
+		$tag_link = get_tag_link( $tag->term_id );
+
+		$html = '<div class="col-6 md-col-4 lg-col-3 p2">';
+			$html .= '<a href="{$tag_link}">';
+				$html .= '{$tag->name}';
+			$html .= '</a>';
+		$html .= '</div>';
+	}
+	echo $html;
+}
