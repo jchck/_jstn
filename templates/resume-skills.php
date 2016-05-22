d6ffdcb667a14f4232ef422374c734dd39aa1f69
jchck <hey@justinchick.com>
2016-05-22 16:18:17 -0400

    clean up the template

--- templates/resume-skills.php
+++ templates/resume-skills.php
@@ -7,53 +7,5 @@ use Jchck\Tags;
 	<h3 class="h1 border-top mt0 pt2 bold">Skills Overview</h3>
 </div>
 <article class="col-12 sm-col-6 pb4">
-
-	<!-- <div class="flex flex-wrap"> -->
-
-		<?= Tags\all_tags(); ?>
-
-		<!-- <div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">HTML</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">CSS</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">Sass</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">Less</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">PostCss</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">PHP</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">JavaScript</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">jQuery</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">Gulp</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">Grunt</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">MySQL</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">git</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">Bower</p>
-		</div>
-		<div class="col-6 md-col-4 lg-col-3 p2">
-			<p class="m0">npm</p>
-		</div> -->
-	<!-- </div> -->
-
+	<?= Tags\all_tags(); ?>
 </article>
