358bb13f4efd5041adc7753b2b0ef2f83c445285
jchck <hey@justinchick.com>
2016-05-24 16:37:33 -0400

    edit to footer copy

--- templates/footer.php
+++ templates/footer.php
@@ -3,7 +3,9 @@
 		<a href="<?= esc_url(home_url('/ck')); ?>">
 			<?php get_template_part( 'templates/logo', 'jc' ); ?>
 		</a>
-		<p style="margin-top:.5rem" class="h5">I'm currently looking for full-time work in New England.<br />If you're intrested in working together drop me a line:</p>
+		<p style="margin-top: .5rem" class="h5">I'm a WordPress developer located in Williamstown, MA.<br />
+			I'm currently seeking full-time work throughout New England.<br />
+			If you're intrested in working together, please drop me a line:</p>
 		<a href="mailto:hey@justinchick.com" class="h3">hey@justinchick.com</a>
 	</div>
 	<?php
