3517487da2354c4a06a8867498142505bf91823c
jchck <hey@justinchick.com>
2016-05-24 19:29:40 -0400

    add amp tag

--- base.php
+++ base.php
@@ -5,7 +5,7 @@ use Jchck\Wrapper;
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html amp <?php language_attributes(); ?>>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
