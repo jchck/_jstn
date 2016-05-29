0cb2cf1b33a8f6f19a3f1162c3d942689cd1a35c
jchck <hey@justinchick.com>
2016-05-29 14:48:57 -0400

    top section

--- template-landing-seo.php
+++ template-landing-seo.php
@@ -8,4 +8,27 @@
 
 	<?php get_template_part( 'templates/header', 'landing' ); ?>
 
+	<div class="col-12 border-top border-width-skinny">
+		<p class="caps mb0">Section Title</p>
+	</div>
+
+	<div class="col-6 my2 sm-my4 pr0 sm-pr2">
+		<h2 class="mt0">Title Here</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+
+	<div class="col-6 my2 sm-my4">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+
 <?php endwhile; ?>
