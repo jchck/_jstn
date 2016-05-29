7fadafc572ebeffe9a95249bc868a3065e9dfa50
jchck <hey@justinchick.com>
2016-05-29 17:16:10 -0400

    no default footer on landing page

--- logic/footer.php
+++ logic/footer.php
@@ -7,6 +7,7 @@ function footer_check(){
 
 	isset($display) || $display = !in_array(true, [
 		is_404(),
+		is_page_template( 'template-landing-seo.php' )
 	]);
 
 	return apply_filters( 'jchck/footer_check', $display );
