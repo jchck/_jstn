19770639882e0d7cf9a6c76d60ac7795a64a96e8
jchck <hey@justinchick.com>
2016-05-29 15:55:50 -0400

    middle and CTA of landing page

--- template-landing-seo.php
+++ template-landing-seo.php
@@ -25,4 +25,24 @@ use Jchck\Pics;
 		<img class="<?= Pics\img_css(); ?>" src="<?php echo get_template_directory_uri() . '/dest/pics/WordPress-Developer-Justin-Chick.jpg'; ?>">
 	</div>
 
+	<div class="col-12 border-top border-width-skinny">
+		<a href="mailto:hey@justinchick.com" style="height:25vh" class="flex items-center justify-center cta">
+			<p class="m0 h2 caps">Let's Work Together</p>
+		</a>
+	</div>
+
+	<div class="col-12 border-top border-width-skinny">
+		<p class="caps mb0">Section Title</p>
+	</div>
+
+	<div class="col-6 my2 sm-my4 pr0 sm-pr2">
+		<img class="<?= Pics\img_css(); ?>" src="<?php echo get_template_directory_uri() . '/dest/pics/custom-wordpress-project.jpg'; ?>">
+	</div>
+
+	<div class="col-6 my2 sm-my4">
+		<h2 class="h1 mt0">Title Here</h2>
+		<p class="h2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+	</div>
+
+
 <?php endwhile; ?>
