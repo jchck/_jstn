e7f047b7bd224a7fdf7ec9b479c255b10a5c0f13
jchck <hey@justinchick.com>
2016-05-30 16:46:37 -0400

    finalize the footer of the landing page

--- templates/footer-landing.php
+++ templates/footer-landing.php
@@ -1,10 +1,10 @@
 <figure class="relative m0 full-width-pic">
+	<figcaption class="absolute mt4 caps col-12 h1 center">Let's Work Together</figcaption>	
 	<img src="<?php echo get_template_directory_uri() . '/dest/pics/custom-wordpress-development.jpg'; ?>">
 </figure>
 
-<footer class="md-px4 px2 bg-black white">
-	<div class="flex items-center justify-between" style="height:10vh">
-		<p class="m0">Handmade by Justin Chick</p>
-		<p class="m0">Lorem ipsum</p>
-	</div>
+<footer class="md-px4 py2 px2 bg-black white flex items-center justify-between">
+	<a href="<?= esc_url(home_url('/ck')); ?>" title="WordPress development in Williamstown Massachusetts" class="flex-auto center">
+		<?php get_template_part( 'templates/logo', 'jc' ); ?>
+	</a>
 </footer>
