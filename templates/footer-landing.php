0babea1c5fe659cf6ede0c3499686d9f966078da
jchck <hey@justinchick.com>
2016-05-30 10:42:01 -0400

    style the landing page footer

--- templates/footer-landing.php
+++ templates/footer-landing.php
@@ -1,1 +1,10 @@
-<h1>Footer Landing</h1>
+<figure class="relative m0 full-width-pic">
+	<img src="<?php echo get_template_directory_uri() . '/dest/pics/custom-wordpress-development.jpg'; ?>">
+</figure>
+
+<footer class="md-px4 px2 bg-black white">
+	<div class="flex items-center justify-between" style="height:10vh">
+		<p class="m0">Handmade by Justin Chick</p>
+		<p class="m0">Lorem ipsum</p>
+	</div>
+</footer>
