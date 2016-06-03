c63c9e05c2904773ea9dc1376e5d56dd563970ab
jchck <hey@justinchick.com>
2016-06-02 21:59:29 -0400

    add unscoped max width

--- single.php
+++ single.php
@@ -7,7 +7,7 @@ use Jchck\Video;
 ?>
 
 <?php while ( have_posts() ): the_post(); ?>
-	<header class="sm-col-6 sm-pr3">
+	<header class="col-12 sm-col-6 sm-pr3">
 		<?php get_template_part( 'templates/page', 'title' ); ?>
 		<time class="caps block mb2" datetime="<?= get_post_time('c', true); ?>">
 			<?= get_the_date('F j, Y'); ?>
@@ -15,7 +15,7 @@ use Jchck\Video;
 		<?= Excerpt\excerpt(); ?>
 		<?= Category\categories(); ?>
 	</header>
-	<article class="sm-col-6">
+	<article class="col-12 sm-col-6">
 		<?= Video\video(); ?>
 		<?php get_template_part( 'templates/content', 'page' ); ?>
 	</article>
