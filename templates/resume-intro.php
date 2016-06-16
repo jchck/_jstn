eb62076dcb7cf9aca75db2ebf4f4e6a55bd9a475
jchck <hey@justinchick.com>
2016-06-16 14:13:15 -0400

    match callouts

--- templates/resume-intro.php
+++ templates/resume-intro.php
@@ -1,6 +1,6 @@
 <header class="col-12 sm-col-6 sm-pr3 mb2 sm-mb0">
 	<?php get_template_part( 'templates/page', 'title' ); ?>
-	<p class="h3 mb1">I'm currently looking for full-time work in New England.<br />If you're intrested in working together drop me a line:</p>
+	<p class="h3 mb1">I'm a front-end developer seeking full-time work.<br />If you're interested in working together, please drop me a line:</p>
 	<p class="h3 sm-mt0 mb2 mb1 caps"><a href="mailto:hey@justinchick.com">hey@justinchick.com</a></p>
 	<p class="sm-m0"><a href="https://www.dropbox.com/s/ivpyov2j2ycazhz/2016-05_jChck_Resume.pdf?dl=0" target="_blank">Download PDF Resume</a></p>
 </header>
