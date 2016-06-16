707f833fcfe4b5de8e3cf1d1c369c956528012cc
jchck <hey@justinchick.com>
2016-06-16 14:16:20 -0400

    match HTML resume with PDF version

--- templates/resume-intro.php
+++ templates/resume-intro.php
@@ -5,8 +5,9 @@
 	<p class="sm-m0"><a href="https://www.dropbox.com/s/ivpyov2j2ycazhz/2016-05_jChck_Resume.pdf?dl=0" target="_blank">Download PDF Resume</a></p>
 </header>
 <article class="col-12 sm-col-6 pb4">
-	<p class="measure">I’m a front-end developer with over 6 years experience in the WordPress ecosystem.</p>
+	<p class="measure">I'm a tried and true front-end developer with 6 + years experience.</p>
 	<p class="measure">I believe in simple, extendable, and reusable code.</p>
 	<p class="measure">I believe in design that lets users do what they need to. Quickly.</p>
 	<p class="measure">Collaboration, exploration, and old fashioned elbow grease intersect on every project I undertake. It's at this intersection that I craft thoughtful, holistic design solutions.</p>
+	<p class="measure">My favorite tools to get the job done are: HTML, CSS, npm, Gulp.js, a modern web browser, pen, and paper.</p>
 </article>
