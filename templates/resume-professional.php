a45cdc7d6abba84c3f05c9f16d5e10353712f3b3
jchck <hey@justinchick.com>
2016-09-21 19:44:30 -0400

    updates to resume

--- templates/resume-professional.php
+++ templates/resume-professional.php
@@ -2,13 +2,21 @@
 	<h2 class="h1 border-top mt0 pt2 bold">Professional Overview</h2>
 </div>
 <article class="col-12 sm-col-6 pb4">
-	<h3 class="h2 bold mt0">Full-Time Freelance</h3>
+
+	<h3 class="h2 bold mt0">Lead Interactive Designer - Hanley Wood</h3>
+	<time class="caps">Sep 2016 - Current</time>
+	<p class="measure mt0">I coordinate between the design, editorial, and back-end development teams to oversee the front-end of all <a href="http://www.hanleywood.com">Hanley Wood</a> web properties including <a href="http://www.architectmagazine.com/">Architect</a>, <a href="http://www.builderonline.com/">Builder</a> and <a href="http://www.remodeling.hw.net/">Remodeling</a> Magazines.</p>
+
+	<h3 class="h2 bold">Freelance</h3>
 	<time class="caps">March 2010 - Current</time>
-	<p class="measure">I oversee projects with organizations like <a href="<?= esc_url(home_url('/case-studies/wash-u-med-school')); ?>">Washington University Med School</a> and <a href="<?= esc_url(home_url('/case-studies/dashtaki-law-firm')); ?>">The Dashtaki Law Firm</a> (and many amazing folks in between) to craft custom WordPress solutions so they can achieve their goals.</p>
-	<h3 class="h2 bold">Development Lead & Partner - Our Collective</h3>
+	<p class="measure mt0">I oversee projects with organizations like <a href="<?= esc_url(home_url('/case-studies/wash-u-med-school')); ?>">Washington University Med School</a> and <a href="<?= esc_url(home_url('/case-studies/dashtaki-law-firm')); ?>">The Dashtaki Law Firm</a> (and many amazing folks in between) to craft custom WordPress solutions so they can achieve their goals.</p>
+
+	<h3 class="h2 bold">Development Lead &amp; Partner - Our Collective</h3>
 	<time class="caps">Dec 2014 - Sep 2015</time>
 	<p class="mt0 measure">Our Collective was a design consultancy I co-founded early in 2015. I oversaw client strategy and development. Nearly all our projects included a custom WordPress build which I oversaw in it's entirety.</p>
-	<h3 class="h2 bold">PR Firestarter - Eureka Interactive</h3>
+
+	<!-- <h3 class="h2 bold">PR Firestarter - Eureka Interactive</h3>
 	<time class="caps">Aug 2010 - Jan 2011</time>
-	<p class="mt0 measure">I established Eureka Interactive’s online marketing offerings, oversaw strategy development and process implementation of client search marketing and social media campaigns.</p>
+	<p class="mt0 measure">I established Eureka Interactive’s online marketing offerings, oversaw strategy development and process implementation of client search marketing and social media campaigns.</p> -->
+
 </article>
