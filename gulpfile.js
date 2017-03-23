686834e74e2b042587320e8b33b171888aac4230
jchck <jchick@hanleywood.com>
2017-03-23 15:06:48 -0500

    them js gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var jshunt = require('gulp-jshint');
+var jshint = require('gulp-jshint');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var util = require('gulp-util');
@@ -8,7 +8,42 @@ var pump = require('pump');
 
 var input = {
 	'js': [
-		'./themes/donuts/source/js/*.js',
-		'./themes/donuts/source/js/_scripts.js'
+		'./themes/donuts/source/_js/*.js',
+		'./themes/donuts/source/_js/_scripts.js'
 	]
 };
+
+var output = {
+	'js' : './themes/donuts/source/js'
+};
+
+/*
+	$ gulp js-concat
+	$ gulp js-concat --type min
+*/
+gulp.task('js-concat', function(cb) {
+	pump([
+		gulp.src(input.js),
+		sourcemaps.init(),
+		concat('scripts.js'),
+			util.env.type === 'min' ? uglify() : util.noop(),
+		sourcemaps.write(),
+		gulp.dest(output.js)
+	], cb);
+});
+
+/*
+	$ gulp js-hint
+*/
+gulp.task('js-hint', ['js-concat'], function() {
+	return gulp.src(input.js)
+		.pipe(jshint.extract('auto'))
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(jshint.reporter('fail'))
+});
+
+/*
+	$ gulp js
+*/
+gulp.task('js', ['js-hint', 'js-concat']);
