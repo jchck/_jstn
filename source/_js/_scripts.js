4210ed018643cfa2a611de79eaddc6be30e76d1e
jchck <hey@justinchick.com>
2017-03-25 14:15:57 -0500

    remove test

--- source/_js/_scripts.js
+++ source/_js/_scripts.js
@@ -3,12 +3,3 @@
 
 	This file will always be the last to be concatenated
 */
-
-function init() {
-  Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
-                   callback: function(data, tabletop) { 
-                       console.log(data);
-                   },
-                   simpleSheet: true } );
-}
-window.addEventListener('DOMContentLoaded', init);
